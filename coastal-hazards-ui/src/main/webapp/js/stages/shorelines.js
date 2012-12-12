1f1ba26f43e7e5f93613a89a1bca9a1a2808953c
Ivan Suftin <isuftin@usgs.gov>
2012-12-12 14:41:06 -0600

    Moved uploader specifically to shoreline js, will duplicate for baseline and transects

--- coastal-hazards-ui/src/main/webapp/js/stages/shorelines.js
+++ coastal-hazards-ui/src/main/webapp/js/stages/shorelines.js
@@ -1,9 +1,10 @@
+// TODO - Back end and front-end verification for uploaded shapefiles
+
 var Shorelines = {
-    
     addShorelines : function() {
-	var coloredShorelines = Object.extended({});
+        var coloredShorelines = Object.extended({});
 	
-	var colorFeatures = function(divId) {
+        var colorFeatures = function(divId) {
             return function(event) {
                 //			var wasEmpty = Object.isEmpty(coloredShorelines);
                 event.features.each(function(el, i, arr) {
@@ -46,12 +47,12 @@ var Shorelines = {
                 //			if (!wasEmpty) {
                 $("#color-legend").html(html.join(''));
                 $("table.tablesorter").tablesorter();
-                //			}
+            //			}
             };
-	};
+        };
 	
-	var layer = [];
-	//We lost features from ASIS
+        var layer = [];
+        //We lost features from ASIS
         //	layer[1] = new OpenLayers.Layer.Vector("WFS1", {
         //                strategies: [new OpenLayers.Strategy.BBOX()],
         //                protocol: new OpenLayers.Protocol.WFS({
@@ -63,7 +64,7 @@ var Shorelines = {
         //            });
         //	layer[1].events.register("featuresadded", null, colorFeatures());
 
-	layer[2] = new OpenLayers.Layer.Vector("WFS2", {
+        layer[2] = new OpenLayers.Layer.Vector("WFS2", {
             strategies: [new OpenLayers.Strategy.BBOX()],
             protocol: new OpenLayers.Protocol.WFS({
                 url:  "geoserver/sample/wfs",
@@ -72,11 +73,85 @@ var Shorelines = {
                 geometryName: "the_geom"
             })
         });
-	layer[2].events.register("featuresadded", null, colorFeatures());
+        layer[2].events.register("featuresadded", null, colorFeatures());
 
         //	map.addLayer(layer[1]);
         map.getMap().addLayer(layer[2]);
 	
+    },
+    initializeShorelineUploader : function() {
+        var uploader = new qq.FineUploader({
+            element: document.getElementById('shoreline-uploader'),
+            request: {
+                endpoint: 'server/upload'
+            },
+            validation: {
+                allowedExtensions: ['zip']
+            },
+            multiple : false,
+            autoUpload: true,
+            text: {
+                uploadButton: '<i class="icon-upload icon-white"></i>Upload Shorelines'
+            },
+            template: '<div class="qq-uploader span4">' +
+            '<pre class="qq-upload-drop-area span4"><span>{dragZoneText}</span></pre>' +
+            '<div class="qq-upload-button btn btn-success" style="width: auto;">{uploadButtonText}</div>' +
+            '<ul class="qq-upload-list hidden" style="margin-top: 10px; text-align: center;"></ul>' +
+            '</div>',
+            classes: {
+                success: 'alert alert-success',
+                fail: 'alert alert-error'
+            },
+            debug: true,
+            callbacks: {
+                onComplete: function(id, fileName, responseJSON) {
+                    if (responseJSON.success) {
+                        if (responseJSON.success != 'true') {
+                            LOG.info('FAIL!!!')
+                        } else {
+                            LOG.info("file-token :" + responseJSON['file-token']);
+                        
+                            permSession.addFileToSession({
+                                token : responseJSON['file-token'], 
+                                name : responseJSON['file-name']
+                            });
+                            permSession.save();
+                            var geoserver = new Geoserver();
+                            var importName = permSession.getCurrentSessionKey() + '_' + responseJSON['file-name'].split('.')[0] + '_shorelines';
+                            var importArgs = {
+                                token : responseJSON['file-token'],
+                                importName : importName, 
+                                workspace : 'ch-input',
+                                callbacks : [function(data) {
+                                    new Geoserver().getCapabilities({
+                                        callbacks : [
+                                        function(caps) {
+                                            $('#shorelines-list').children().remove();
+                                            $(caps.featureTypeList.featureTypes).each(function(index, item, arr) { 
+                                                var title = item.title;
+                                                var shortenedTitle = title.has(permSession.getCurrentSessionKey()) ? 
+                                                title.remove(permSession.getCurrentSessionKey() + '_') : 
+                                                title;
+                                                if (title.has(permSession.getCurrentSessionKey()));
+                                                if (title.substr(title.lastIndexOf('_') + 1) == 'shorelines') {
+                                                    $('#shorelines-list')
+                                                    .append($("<option></option>")
+                                                        .attr("value",title)
+                                                        .text(shortenedTitle));
+                                                } 
+                                            });
+                                        }
+                                        ]
+                                    })
+                                //                                
+                                }]
+                            }
+                            geoserver.importFile(importArgs);
+                        }
+                    }
+                }
+            }
+        })
     }
     
 }
--- coastal-hazards-ui/src/main/webapp/pages/index/onReady.js
+++ coastal-hazards-ui/src/main/webapp/pages/index/onReady.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
         LOG4JS_LOG_THRESHOLD : CONFIG.development ? 'debug' : 'info'
     });
     
+    Shorelines.initializeShorelineUploader();
         
     ui = new UI();
     map = new Map();
@@ -15,6 +16,7 @@ $(document).ready(function() {
     // Set up sessions
     tempSession = new Session('coastal-hazards', false);
     permSession = new Session('coastal-hazards', true);
+    
     LOG.info('Sessions created. User session list has ' + Object.keys(permSession.session.sessions).length + ' sessions.')
     LOG.info('Current session key: ' + permSession.getCurrentSessionKey());
 	
@@ -23,84 +25,5 @@ $(document).ready(function() {
     $("#calculate-transects-btn").on("click", Transects.calcTransects);
     $("#create-intersections-btn").on("click", Intersections.calcIntersections);
     $("#display-results-btn").on("click", function() { /* not yet implemented */});
-    
-    
-    //Initialize the uploader
-    var uploader = new qq.FineUploader({
-        element: document.getElementById('shoreline-uploader'),
-        request: {
-            endpoint: 'server/upload'
-        },
-        validation: {
-            allowedExtensions: ['zip']
-        },
-        multiple : false,
-        autoUpload: true,
-        text: {
-            uploadButton: '<i class="icon-upload icon-white"></i>Upload A File'
-        },
-        template: '<div class="qq-uploader span4">' +
-        '<pre class="qq-upload-drop-area span4"><span>{dragZoneText}</span></pre>' +
-        '<div class="qq-upload-button btn btn-success" style="width: auto;">{uploadButtonText}</div>' +
-        '<ul class="qq-upload-list hidden" style="margin-top: 10px; text-align: center;"></ul>' +
-        '</div>',
-        classes: {
-            success: 'alert alert-success',
-            fail: 'alert alert-error'
-        },
-        debug: true,
-        callbacks: {
-            onComplete: function(id, fileName, responseJSON) {
-                if (responseJSON.success) {
-                    if (responseJSON.success != 'true') {
-                        LOG.info('FAIL!!!')
-                    } else {
-                        LOG.info("file-token :" + responseJSON['file-token']);
-                        
-                        permSession.addFileToSession({
-                            token : responseJSON['file-token'], 
-                            name : responseJSON['file-name']
-                        });
-                        permSession.save();
-                        var geoserver = new Geoserver();
-                        var importName = permSession.getCurrentSessionKey() + '_' + responseJSON['file-name'].split('.')[0] + '_shorelines';
-                        var importArgs = {
-                            token : responseJSON['file-token'],
-                            importName : importName, 
-                            workspace : 'ch-input',
-                            callbacks : [function(data) {
-                                new Geoserver().getCapabilities({
-                                    callbacks : [
-                                    function(caps) {
-                                        $('#shorelines-list').children().remove();
-                                        $(caps.featureTypeList.featureTypes).each(function(index, item, arr) { 
-                                            var title = item.title;
-                                            var shortenedTitle = title.has(permSession.getCurrentSessionKey()) ? 
-                                            title.remove(permSession.getCurrentSessionKey() + '_') : 
-                                            title;
-                                            if (title.has(permSession.getCurrentSessionKey()));
-                                            if (title.substr(title.lastIndexOf('_') + 1) == 'shorelines') {
-                                                $('#shorelines-list')
-                                                .append($("<option></option>")
-                                                    .attr("value",title)
-                                                    .text(shortenedTitle));
-                                            } 
-                                        });
-                                    }
-                                    ]
-                                })
-                            //                                
-                            }]
-                        }
-                        geoserver.importFile(importArgs);
-                    }
-                }
-            }
-        }
-    })
-    
-    $('#myModal-save-btn').click(function() {
-        uploader.uploadStoredFiles();
-    });
     
 })
