b876ae213f5e277d1eb5123390bf738033cb9d5c
Ivan Suftin <isuftin@usgs.gov>
2012-12-20 19:18:48 -0600

    Baselines are now showing up on the map when drawn. Auto-selected in select box and auto-shown on the map

--- coastal-hazards-ui/src/main/webapp/js/stages/baseline.js
+++ coastal-hazards-ui/src/main/webapp/js/stages/baseline.js
@@ -37,87 +37,96 @@ var Baseline = {
     clearDrawFeatures : function() {
         return map.getMap().getControlsBy('id','baseline-draw-control')[0].layer.removeAllFeatures();
     },
-    saveDrawnFeatures : function() {
-        var drawLayer = map.getMap().getControlsBy('id','baseline-draw-control')[0].layer;
-        var importName = tempSession.getCurrentSessionKey() + '_' + ($('#baseline-draw-form-name').val() || Util.getRandomLorem()) + '_baseline';
-        
-        if (drawLayer.features.length && importName) {
-            LOG.info('User has drawn a feature and is saving it');
-            geoserver.importFile({
-                token : '',
-                importName : importName, 
-                workspace : 'ch-input',
-                context : drawLayer,
-                callbacks : [function(data, context) {
-                    if (data.success === 'true') {
-                        LOG.info('File imported successfully - reloading current file set from server');
-                        var layerName = data.feature;
-                        var drawControl = map.getMap().getControlsBy('id','baseline-draw-control')[0];
-                        var schema = context.protocol.schema;
-                        var newSchema = schema.substring(0, schema.lastIndexOf(':') + 1) + importName;
-            
-                        context.protocol.setFeatureType(importName);
-                        context.protocol.format.options.schema = newSchema;
-                        context.protocol.schema = newSchema;
-                        
-                        // Do WFS-T to fill out the layer
-                        var saveStrategy = context.strategies.find(function(n) {
-                            return n['CLASS_NAME'] == 'OpenLayers.Strategy.Save'
-                        });
-                        
-                        saveStrategy.events.remove('success');
-
-                        saveStrategy.events.register('success', null, function() {
-                            // GDP requires having at least one attribute value, so send an update
-                            // transaction to set the 'ID' attribute to 0 for the newly created geometry.
-                            geoserver.updateFeatureTypeAttribute(layerName, 'ID', 0, function(data) {
-                                var a = 1;
-                            });
-                            
-                        });
-
-                        saveStrategy.save();
-
-                    //                        geoserver.getWMSCapabilities({
-                    //                            callbacks : [
-                    //                            function (data) {
-                    //                                ui.populateFeaturesList(data, context);
-                    //                                tempSession.addFileToSession(data);
-                    //                            // TODO - add the layer just imported 
-                    //                            }
-                    //                            ]
-                    //                        })
-                    } else {
-                        // TODO - Notify the user
-                        LOG.warn(data.error);
+    refreshFeatureList : function(args) {
+        var selectLayer = args.selectLayer; 
+        geoserver.getWMSCapabilities({
+            selectLayer : selectLayer,
+            callbacks : [
+            function(caps, context) {
+                Baseline.populateFeaturesList(caps);
+            },
+            function(caps, context) {
+                $('#baseline-list').children().each(function(i,v) {
+                    if (v.value === 'ch-input:' + selectLayer) {
+                        LOG.debug('Triggering "select" on featurelist option');
+                        $('#baseline-list').val(v.value);
+                        $('#baseline-list').trigger('change');
                     }
-                }]
-            });
-        } else {
-            LOG.info('User has not drawn any features to save or did not name the new feature');
+                })
         }
-    },
-    createDrawPanel : function() {
-        var well = $('<div />').attr('id', 'draw-panel-well').addClass('well');
-        var fluidContainer = $('<div />').attr('id', 'draw-panel-container').addClass('container-fluid');
-        var rows = [];
-        rows.push( 
-            $('<div />').addClass('row-fluid span12').append(
-                // Baseline Name
-                $('<input />').addClass('input-xlarge span6').attr('id', 'baseline-draw-form-name').val(Util.getRandomLorem()).
-                before($('<label />').addClass('control-label').attr('for', 'baseline-draw-form-name').html('Baseline Name'))
-                ),
-            $('<div />').addClass('row-fluid span12').append(
-                // Baseline Name
-                $('<button />').addClass('btn').attr('id', 'baseline-draw-form-save').html('Save').on('click', Baseline.saveDrawnFeatures).
-                after($('<button />').addClass('btn').attr('id', 'baseline-draw-form-clear').html('Clear').on('click', Baseline.clearDrawFeatures))
-                )
-            )
-        
-        $(rows).each(function(i,row) {
-            fluidContainer.append(row);
+        ]
         })
+},
+saveDrawnFeatures : function() {
+    var drawLayer = map.getMap().getControlsBy('id','baseline-draw-control')[0].layer;
+    var importName = tempSession.getCurrentSessionKey() + '_' + ($('#baseline-draw-form-name').val() || Util.getRandomLorem()) + '_baseline';
         
-        return well.append(fluidContainer)
+    if (drawLayer.features.length && importName) {
+        LOG.info('User has drawn a feature and is saving it');
+        geoserver.importFile({
+            token : '',
+            importName : importName, 
+            workspace : 'ch-input',
+            context : drawLayer,
+            callbacks : [function(data, context) {
+                if (data.success === 'true') {
+                    LOG.info('File imported successfully - reloading current file set from server');
+                    var layerName = data.feature;
+                    var drawControl = map.getMap().getControlsBy('id','baseline-draw-control')[0];
+                    var schema = context.protocol.schema;
+                    var newSchema = schema.substring(0, schema.lastIndexOf(':') + 1) + importName;
+            
+                    context.protocol.setFeatureType(importName);
+                    context.protocol.format.options.schema = newSchema;
+                    context.protocol.schema = newSchema;
+                        
+                    // Do WFS-T to fill out the layer
+                    var saveStrategy = context.strategies.find(function(n) {
+                        return n['CLASS_NAME'] == 'OpenLayers.Strategy.Save'
+                    });
+                        
+                    saveStrategy.events.remove('success');
+
+                    saveStrategy.events.register('success', null, function() {
+                        Baseline.refreshFeatureList({
+                            selectLayer : importName
+                        })
+                    });
+
+                    saveStrategy.save();
+
+                    
+                } else {
+                    // TODO - Notify the user
+                    LOG.warn(data.error);
+                }
+            }]
+        });
+    } else {
+        LOG.info('User has not drawn any features to save or did not name the new feature');
     }
+},
+createDrawPanel : function() {
+    var well = $('<div />').attr('id', 'draw-panel-well').addClass('well');
+    var fluidContainer = $('<div />').attr('id', 'draw-panel-container').addClass('container-fluid');
+    var rows = [];
+    rows.push( 
+        $('<div />').addClass('row-fluid span12').append(
+            // Baseline Name
+            $('<input />').addClass('input-xlarge span6').attr('id', 'baseline-draw-form-name').val(Util.getRandomLorem()).
+            before($('<label />').addClass('control-label').attr('for', 'baseline-draw-form-name').html('Baseline Name'))
+            ),
+        $('<div />').addClass('row-fluid span12').append(
+            // Baseline Name
+            $('<button />').addClass('btn').attr('id', 'baseline-draw-form-save').html('Save').on('click', Baseline.saveDrawnFeatures).
+            after($('<button />').addClass('btn').attr('id', 'baseline-draw-form-clear').html('Clear').on('click', Baseline.clearDrawFeatures))
+            )
+        )
+        
+    $(rows).each(function(i,row) {
+        fluidContainer.append(row);
+    })
+        
+    return well.append(fluidContainer)
+}
 }
