27674e6717b56af5b74231064a50561f2d3df163
Jordan Walker <jiwalker@usgs.gov>
2013-01-07 00:59:00 -0600

    Almost done with transect creation, need to find some tests to run

--- coastal-hazards-wps/src/main/java/gov/usgs/cida/coastalhazards/wps/GenerateTransectsProcess.java
+++ coastal-hazards-wps/src/main/java/gov/usgs/cida/coastalhazards/wps/GenerateTransectsProcess.java
@@ -1,14 +1,18 @@
 package gov.usgs.cida.coastalhazards.wps;
 
+import com.vividsolutions.jts.algorithm.Angle;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.CoordinateList;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineSegment;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.MultiLineString;
 import gov.usgs.cida.coastalhazards.util.UTMFinder;
 import gov.usgs.cida.coastalhazards.wps.exceptions.UnsupportedCoordinateReferenceSystemException;
 import gov.usgs.cida.coastalhazards.wps.exceptions.UnsupportedFeatureTypeException;
+import java.util.LinkedList;
+import java.util.List;
 import org.geoserver.wps.gs.GeoServerProcess;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.feature.FeatureCollection;
@@ -39,7 +43,9 @@ public class GenerateTransectsProcess implements GeoServerProcess {
     
     private static final CoordinateReferenceSystem ACCEPTED_CRS = DefaultGeographicCRS.WGS84;
 
-    /* May actually want to return reference to new layer*/
+    /** May actually want to return reference to new layer
+     *  Check whether we need an offset at the start of the baseline
+     */
     @DescribeResult(name = "transects", description = "Layer containing Transects normal to baseline")
     public int execute(
             @DescribeParameter(name = "shorelines", min = 1, max = 1) SimpleFeatureCollection shorelines,
@@ -52,6 +58,7 @@ public class GenerateTransectsProcess implements GeoServerProcess {
     }
     
     private class Process {
+        private static final int HEURISTIC_LENGTH = 1000;
         
         private final FeatureCollection<SimpleFeatureType, SimpleFeature> shorelines;
         private final FeatureCollection<SimpleFeatureType, SimpleFeature> baseline;
@@ -89,8 +96,10 @@ public class GenerateTransectsProcess implements GeoServerProcess {
                 throw new UnsupportedCoordinateReferenceSystemException("Baseline is not in accepted projection");
             }
             
-            Coordinate[] pointsOnBaseline = getEvenlySpacedPointsAlongBaseline(baseline, spacing);
-            FeatureCollection resultingTransects = getTransectsAtPoints(pointsOnBaseline, baseline, shorelines);
+            // probably want to get UTM zone and pass it in
+            LineString[] baselineGeometry = getLinesFromFeatureCollection(baseline);
+            VectorCoordAngle[] vectsOnBaseline = getEvenlySpacedOrthoVectorsAlongBaseline(baselineGeometry, spacing);
+            FeatureCollection resultingTransects = getTransects(vectsOnBaseline, baseline, shorelines);
             addResultAsLayer(resultingTransects, workspace, store, layer);
             return 0;
         }
@@ -101,17 +110,24 @@ public class GenerateTransectsProcess implements GeoServerProcess {
             CoordinateReferenceSystem coordinateReferenceSystem = sft.getCoordinateReferenceSystem();
             return coordinateReferenceSystem;
         }
-
-        private Coordinate[] getEvenlySpacedPointsAlongBaseline(FeatureCollection<SimpleFeatureType, SimpleFeature> baseline, double spacing) {
-            CoordinateList coordList = new CoordinateList();
+        
+        /**
+         * Need to watch out here, the utm detection is in here, so there is the possibility of using two different projections
+         * @param featureCollection
+         * @return 
+         */
+        private LineString[] getLinesFromFeatureCollection(FeatureCollection<SimpleFeatureType, SimpleFeature> featureCollection) {
+            List<LineString> lines = new LinkedList<LineString>();
             CoordinateReferenceSystem utmCrs = null;
+            MathTransform transform = null;
             try {
-                utmCrs = UTMFinder.findUTMZoneForFeatureCollection((SimpleFeatureCollection)baseline);
+                utmCrs = UTMFinder.findUTMZoneForFeatureCollection((SimpleFeatureCollection)featureCollection);
+                transform = CRS.findMathTransform(ACCEPTED_CRS, utmCrs, true);
             }
             catch (FactoryException ex) {
                 return null; // do something better than this
             }
-            FeatureIterator<SimpleFeature> features = baseline.features();
+            FeatureIterator<SimpleFeature> features = featureCollection.features();
             SimpleFeature feature = null;
             while (features.hasNext()) {
                 feature = features.next();
@@ -119,11 +135,7 @@ public class GenerateTransectsProcess implements GeoServerProcess {
                 
                 Geometry utmGeometry = null;
                 try {
-                    MathTransform transform = CRS.findMathTransform(ACCEPTED_CRS, utmCrs, true);
                     utmGeometry = JTS.transform(geometry, transform);
-                }
-                catch (FactoryException ex) {
-                    // TODO handle exceptions
                 }
                 catch (TransformException ex) {
                     // TODO handle exceptions
@@ -138,15 +150,13 @@ public class GenerateTransectsProcess implements GeoServerProcess {
                         throw new UnsupportedFeatureTypeException("Polygons not supported in baseline");
                     case LINESTRING:
                         lineString = (LineString)utmGeometry;
-                        coords = handleLineString(lineString, spacing);
-                        coordList.add(coords, false);
+                        lines.add(lineString);
                         break;
                     case MULTILINESTRING:
                         MultiLineString multiLineString = (MultiLineString)utmGeometry;
                         for (int i=0; i < multiLineString.getNumGeometries(); i++) {
                             lineString = (LineString)multiLineString.getGeometryN(i);
-                            coords = handleLineString(lineString, spacing);
-                            coordList.add(coords, false);
+                            lines.add(lineString);
                         }
                         break;
                     case POINT:
@@ -155,17 +165,30 @@ public class GenerateTransectsProcess implements GeoServerProcess {
                     default:
                         throw new UnsupportedFeatureTypeException("Only line type supported");
                 }
-                
-                // cast the feature to Line2D
-                // add a coordinate to set
-                // walk the line until the spacing is reached
-                // make another coordinate
-                // keep walking
             }
-            return coordList.toCoordinateArray();
+            LineString[] linesArr = new LineString[lines.size()];
+            return lines.toArray(linesArr);
+        }
+
+        private VectorCoordAngle[] getEvenlySpacedOrthoVectorsAlongBaseline(LineString[] baseline, double spacing) {
+            List<VectorCoordAngle> vectList = new LinkedList<VectorCoordAngle>();
+            
+            for (LineString line : baseline) {
+                vectList.addAll(handleLineString(line, spacing));
+            }
+            VectorCoordAngle[] vectArr = new VectorCoordAngle[vectList.size()];
+            return vectList.toArray(vectArr);
         }
         
-        private FeatureCollection getTransectsAtPoints(Coordinate[] pointsOnBaseline, FeatureCollection<SimpleFeatureType, SimpleFeature> baseline, FeatureCollection<SimpleFeatureType, SimpleFeature> shorelines) {
+        private FeatureCollection getTransects(VectorCoordAngle[] vectsOnBaseline, FeatureCollection<SimpleFeatureType, SimpleFeature> baseline, FeatureCollection<SimpleFeatureType, SimpleFeature> shorelines) {
+            shorelines.
+            for (VectorCoordAngle vect : vectsOnBaseline) {
+                LineString testLine = vect.getLineOfLength(HEURISTIC_LENGTH);
+                if (testLine.intersects(shorelines)) {
+                    
+                }
+            }
+            
             throw new UnsupportedOperationException("Not yet implemented");
             // for each point find the normal to baseline
             // clip normal to furthest shoreline
@@ -177,38 +200,79 @@ public class GenerateTransectsProcess implements GeoServerProcess {
             // return workspace:layerName from gs:Import
         }
 
-        private Coordinate[] handleLineString(LineString lineString, double spacing) {
+        /**
+         * Vectors point 90&deg; counterclockwise currently
+         * @param lineString line along which to get vectors
+         * @param spacing how often to create vectors along line
+         * @return List of fancy vectors I concocted
+         */
+        private List<VectorCoordAngle> handleLineString(LineString lineString, double spacing) {
             Coordinate currentCoord = null;
-            CoordinateList transectPoints = new CoordinateList();
-            double accumulatedDistance = 0l;
-            Coordinate lastCoord = null;
-            for (Coordinate coord : lineString.getCoordinates()) {
-                lastCoord = coord;
+            List<VectorCoordAngle> transectVectors = new LinkedList<VectorCoordAngle>();
+            double accumulatedDistance = 0.0d;
+            for (int i=0; i<lineString.getNumPoints(); i++) {
+                Coordinate coord = lineString.getCoordinateN(i);
                 if (currentCoord == null) {
                     currentCoord = coord;
-                    transectPoints.add(coord, false);
+                    Coordinate nextCoord = lineString.getCoordinateN(i+1);
+                    if (nextCoord == null) {
+                        throw new IllegalStateException("Line must have at least two points");
+                    }
+                    LineSegment segment = new LineSegment(currentCoord, nextCoord);
+                    double orthogonal = segment.angle() + Angle.PI_OVER_4;
+                    VectorCoordAngle vect = new VectorCoordAngle(currentCoord, orthogonal);
+                    transectVectors.add(vect);
                     continue;
                 }
                 double distance = coord.distance(currentCoord);
                 if ((accumulatedDistance + distance) > spacing) {
-
                     double distanceToNewPoint = spacing - accumulatedDistance;
                     double fraction = distanceToNewPoint / distance;
                     LineSegment segment = new LineSegment(currentCoord, coord);
                     Coordinate pointAlong = segment.pointAlong(fraction);
-                    transectPoints.add(pointAlong);
+                    double orthogonal = segment.angle() + Angle.PI_OVER_4;
+                    VectorCoordAngle vect = new VectorCoordAngle(pointAlong, orthogonal);
+                    transectVectors.add(vect);
                     currentCoord = pointAlong;
-                    accumulatedDistance = 0l;
+                    accumulatedDistance = 0.0d;
                 }
                 else {
                     accumulatedDistance += distance;
                     currentCoord = coord;
                 }
             }
-            // TODO does this make sense?
-            transectPoints.add(lastCoord);
-            return transectPoints.toCoordinateArray();
+            return transectVectors;
         }
 
+    }
+    
+    /**
+     * Creating this class because I'm having a hard time wrapping my head 
+     * around polar vs. cartesian arithmetic.  Holding a cartesian coord with
+     * a polar angle seemed to be my best way around it.
+     */
+    private class VectorCoordAngle {
+        
+        private Coordinate cartesianCoord;
+        private double angle;
+        private GeometryFactory gf;
+        
+        private VectorCoordAngle(Coordinate coord, double angle) {
+            this.cartesianCoord = coord;
+            this.angle = angle;
+            this.gf = new GeometryFactory();
+        }
+        
+        private VectorCoordAngle(double x, double y, double angle) {
+            this(new Coordinate(x, y), angle);
+        }
+        
+        private LineString getLineOfLength(double length) {
+            double rise = length * Math.sin(angle);
+            double run = length * Math.cos(angle);
+            Coordinate endpoint = new Coordinate(cartesianCoord.x + run, cartesianCoord.y + rise);
+            LineString newLineString = gf.createLineString(new Coordinate[] {cartesianCoord, endpoint});
+            return newLineString;
+        }
     }
 }
