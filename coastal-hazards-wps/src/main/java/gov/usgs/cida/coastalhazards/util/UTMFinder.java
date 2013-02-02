72adc365fcb5cb674e0107371ca076bf57e6e6ef
Tom Kunicki <tkunicki@usgs.gov>
2013-02-01 23:33:57 -0600

    Transform feature collection envelope to WGS84 before calculating UTM zone

--- coastal-hazards-wps/src/main/java/gov/usgs/cida/coastalhazards/util/UTMFinder.java
+++ coastal-hazards-wps/src/main/java/gov/usgs/cida/coastalhazards/util/UTMFinder.java
@@ -8,9 +8,12 @@ package gov.usgs.cida.coastalhazards.util;
 import com.vividsolutions.jts.geom.Coordinate;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.referencing.CRS;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.NoSuchAuthorityCodeException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.crs.GeographicCRS;
+import org.opengis.referencing.operation.TransformException;
 
 /**
  *
@@ -26,11 +29,11 @@ public class UTMFinder {
      * Need to be in lat/lon (WGS84) for this to work
      * Calculation is based on http://en.wikipedia.org/wiki/UTM_coordinate_system
      * 
-     * @param featureCollection TODO? maybe should put in check for WGS84 again
+     * @param featureCollection
      * @return UTM CRS
      */
-    public static CoordinateReferenceSystem findUTMZoneForFeatureCollection(SimpleFeatureCollection featureCollection) throws NoSuchAuthorityCodeException, FactoryException {
-        Coordinate centre = featureCollection.getBounds().centre();
+    public static CoordinateReferenceSystem findUTMZoneForFeatureCollection(SimpleFeatureCollection featureCollection) throws NoSuchAuthorityCodeException, FactoryException, TransformException {
+        Coordinate centre = featureCollection.getBounds().transform(DefaultGeographicCRS.WGS84, true).centre();
         int utmZone = (int)Math.ceil((180 + centre.x) / 6);
         int epsg = BASE_UTM_EPSG + utmZone + ((centre.y > 0) ? 0 : SOUTHERN_HEMI);
         CoordinateReferenceSystem crs = CRS.decode("EPSG:" + epsg);
