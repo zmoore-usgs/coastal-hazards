b6408cfa11b1a3a1569a33feb5db5184c3936457
Ivan Suftin <isuftin@usgs.gov>
2013-02-13 13:20:55 -0600

    Tried writing attributes to a shapefile another way and it also did not work. This is probably not going to work the way I am doing it and I will end up going back and writing a new featurecollection, importing it with the added attributes, deleting the old fc and renaming the new one to the old one

--- coastal-hazards-wps/src/main/java/gov/usgs/cida/coastalhazards/wps/AppendColumnsToLayerProcess.java
+++ coastal-hazards-wps/src/main/java/gov/usgs/cida/coastalhazards/wps/AppendColumnsToLayerProcess.java
@@ -4,27 +4,34 @@ import gov.usgs.cida.coastalhazards.util.LayerImportUtil;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.geoserver.catalog.AttributeTypeInfo;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.catalog.DataStoreInfo;
 import org.geoserver.catalog.WorkspaceInfo;
-import org.geoserver.catalog.impl.AttributeTypeInfoImpl;
 import org.geoserver.wps.gs.GeoServerProcess;
 import org.geoserver.wps.gs.ImportProcess;
+import org.geotools.data.AbstractDataStore;
+import org.geotools.data.DataAccess;
+import org.geotools.data.DataStore;
+import org.geotools.data.FeatureSource;
 import org.geotools.data.directory.DirectoryDataStore;
 import org.geotools.data.shapefile.ShapefileDataStore;
 import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureStore;
 import org.geotools.feature.AttributeTypeBuilder;
+import org.geotools.feature.NameImpl;
 import org.geotools.feature.simple.SimpleFeatureTypeImpl;
-import org.geotools.feature.type.AttributeTypeImpl;
+import org.geotools.feature.type.FeatureTypeImpl;
 import org.geotools.process.ProcessException;
 import org.geotools.process.factory.DescribeParameter;
 import org.geotools.process.factory.DescribeProcess;
 import org.geotools.process.factory.DescribeResult;
 import org.geotools.util.DefaultProgressListener;
+import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.AttributeDescriptor;
-import org.opengis.feature.type.AttributeType;
+import org.opengis.feature.type.FeatureType;
+import org.opengis.feature.type.Name;
+import org.opengis.feature.type.PropertyDescriptor;
 
 /**
  *
@@ -52,7 +59,6 @@ public class AppendColumnsToLayerProcess implements GeoServerProcess {
             @DescribeParameter(name = "column", min = 1, max = Integer.MAX_VALUE, description = "Column Name|Column Type|Column Description|Default Value") String[] columns)
             throws ProcessException {
 
-        String layerName = sfc.getSchema().getTypeName();
         WorkspaceInfo ws;
         ws = catalog.getWorkspaceByName(workspace);
         if (ws == null) {
@@ -64,8 +70,17 @@ public class AppendColumnsToLayerProcess implements GeoServerProcess {
             throw new ProcessException("Could not find store " + store + " in workspace " + workspace);
         }
 
-        SimpleFeatureType sft = sfc.getSchema();
-        List<AttributeDescriptor> attributeDescriptors = new ArrayList(sft.getAttributeDescriptors());
+        FeatureSource featureSource;
+        try {
+            featureSource = storeInfo.getDataStore(new DefaultProgressListener()).getFeatureSource(new NameImpl(sfc.getSchema().getTypeName()));
+        } catch (IOException ioe) {
+            throw new ProcessException(ioe);
+        }
+        
+        SimpleFeatureStore sstore = (SimpleFeatureStore) featureSource;
+        
+        FeatureType ft = featureSource.getSchema();
+        List<PropertyDescriptor> propertyDescriptors = new ArrayList(ft.getDescriptors());
 
         for (String column : columns) {
             String[] columnAttributes = column.split("\\|");
@@ -76,33 +91,32 @@ public class AppendColumnsToLayerProcess implements GeoServerProcess {
             String type = columnAttributes[1];
             String description = columnAttributes[2];
             String defaultValue = columnAttributes[3];
-            if (sft.getDescriptor(column) == null) {
+            if (ft.getDescriptor(column) == null) {
                 AttributeTypeBuilder atb = new AttributeTypeBuilder();
-                
+
                 atb.setName(name);
                 atb.setMinOccurs(0);
                 atb.setMaxOccurs(1);
                 atb.setBinding(String.class);
                 atb.setDefaultValue(defaultValue);
                 AttributeDescriptor descriptor = atb.buildDescriptor(description);
-                attributeDescriptors.add(descriptor);
+                propertyDescriptors.add(descriptor);
             }
         }
 
-        SimpleFeatureType ft = new SimpleFeatureTypeImpl(
-                sft.getName(),
-                attributeDescriptors,
-                sft.getGeometryDescriptor(),
-                sft.isAbstract(),
-                sft.getRestrictions(),
-                sft.getSuper(),
-                sft.getDescription());
+        FeatureTypeImpl newFt = new FeatureTypeImpl(
+                ft.getName(),
+                propertyDescriptors,
+                ft.getGeometryDescriptor(),
+                ft.isAbstract(),
+                ft.getRestrictions(),
+                ft.getSuper(),
+                ft.getDescription());
         try {
-            DefaultProgressListener progressListener = new DefaultProgressListener();
-            DirectoryDataStore sds = (DirectoryDataStore) storeInfo.getDataStore(progressListener);
-            sds.updateSchema(sft.getName(), ft);
+            DataAccess sds = featureSource.getDataStore();
+            sds.updateSchema(newFt.getName(), ft);
         } catch (IOException ex) {
-            throw new ProcessException("column input must have four attributes split by a pipe character: \"Column Name|Column Type|Column Description|Default Value\"");
+            throw new ProcessException(ex);
         }
 
         return null;
