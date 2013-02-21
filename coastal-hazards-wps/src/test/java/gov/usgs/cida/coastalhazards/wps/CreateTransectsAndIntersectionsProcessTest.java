a270fd326ace1db7f68696624fe3f6cfc71d1536
Jordan Walker <jiwalker@usgs.gov>
2013-02-20 18:51:45 -0600

    Had the test ignored, didn't mean to do that

--- coastal-hazards-wps/src/test/java/gov/usgs/cida/coastalhazards/wps/CreateTransectsAndIntersectionsProcessTest.java
+++ coastal-hazards-wps/src/test/java/gov/usgs/cida/coastalhazards/wps/CreateTransectsAndIntersectionsProcessTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gov.usgs.cida.coastalhazards.wps;
 
 import com.vividsolutions.jts.algorithm.Angle;
@@ -14,6 +10,7 @@ import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.PrecisionModel;
 import com.vividsolutions.jts.geom.prep.PreparedGeometry;
 import com.vividsolutions.jts.geom.prep.PreparedGeometryFactory;
+import gov.usgs.cida.coastalhazards.util.BaselineDistanceAccumulator;
 import gov.usgs.cida.coastalhazards.util.FeatureCollectionFromShp;
 import java.io.File;
 import java.net.URL;
@@ -398,7 +395,7 @@ public class CreateTransectsAndIntersectionsProcessTest {
             new Coordinate(8, 0),
             new Coordinate(9, 0),
         });
-        LineSegment start = CreateTransectsAndIntersectionsProcess.getStartLineSegment(lineString);
+        LineSegment start = BaselineDistanceAccumulator.getStartLineSegment(lineString);
         assertNotNull(start);
         assertEquals(lineString.getCoordinateN(0), start.getCoordinate(0));
         assertEquals(lineString.getCoordinateN(1), start.getCoordinate(1));
@@ -418,7 +415,7 @@ public class CreateTransectsAndIntersectionsProcessTest {
             new Coordinate(8, 0),
             new Coordinate(9, 0),
         });
-        LineSegment end = CreateTransectsAndIntersectionsProcess.getEndLineSegment(lineString);
+        LineSegment end = BaselineDistanceAccumulator.getEndLineSegment(lineString);
         assertNotNull(end);
         assertEquals(lineString.getCoordinateN(8), end.getCoordinate(0));
         assertEquals(lineString.getCoordinateN(9), end.getCoordinate(1));
@@ -432,42 +429,42 @@ public class CreateTransectsAndIntersectionsProcessTest {
         
         end = new LineSegment(0, 0, 1, 0);
         start = new LineSegment(1, 0, 2, 0);
-        distance = CreateTransectsAndIntersectionsProcess.getMinimumProjectedDistance(end, start);
+        distance = BaselineDistanceAccumulator.getMinimumProjectedDistance(end, start);
         assertEquals("abutting segments along same line", 0, distance, EPS);
         
         end = new LineSegment(0, 0, 1, 0);
         start = new LineSegment(3, 0, 4, 0);
-        distance = CreateTransectsAndIntersectionsProcess.getMinimumProjectedDistance(end, start);
+        distance = BaselineDistanceAccumulator.getMinimumProjectedDistance(end, start);
         assertEquals("segments along same line (discontinuous)", 2, distance, EPS);
         
         end = new LineSegment(0, 1, 1, 1);
         start = new LineSegment(1, 1, 2, 1);
-        distance = CreateTransectsAndIntersectionsProcess.getMinimumProjectedDistance(end, start);
+        distance = BaselineDistanceAccumulator.getMinimumProjectedDistance(end, start);
         assertEquals("segments parallel to same line (abutting along projection)", 0, distance, EPS);
         
         end = new LineSegment(0, 1, 1, 1);
         start = new LineSegment(3, 1, 4, 1);
-        distance = CreateTransectsAndIntersectionsProcess.getMinimumProjectedDistance(end, start);
+        distance = BaselineDistanceAccumulator.getMinimumProjectedDistance(end, start);
         assertEquals("segments parallel to same line (discontinuous along projection)", 2, distance, EPS);
         
         end = new LineSegment(0, 0, 1, 0);
         start = new LineSegment(1, 0, 1, 1);
-        distance = CreateTransectsAndIntersectionsProcess.getMinimumProjectedDistance(end, start);
+        distance = BaselineDistanceAccumulator.getMinimumProjectedDistance(end, start);
         assertEquals("segments perpendicular to each other (abutting at end)", 0, distance, EPS);
         
         end = new LineSegment(0, 0, 1, 0);
         start = new LineSegment(1, 2, 1, 4);
-        distance = CreateTransectsAndIntersectionsProcess.getMinimumProjectedDistance(end, start);
+        distance = BaselineDistanceAccumulator.getMinimumProjectedDistance(end, start);
         assertEquals("segments perpendicular to each other (discontinuos at end)", 0, distance, EPS);
         
         end = new LineSegment(0, 0, 1, 0);
         start = new LineSegment(2, 2, 2, 3);
-        distance = CreateTransectsAndIntersectionsProcess.getMinimumProjectedDistance(end, start);
+        distance = BaselineDistanceAccumulator.getMinimumProjectedDistance(end, start);
         assertEquals("segments perpendicular to each other (offset 1 in x, 2 in y)", 1, distance, EPS);
         
         end = new LineSegment(-1, 0, 0, 0);
         start = new LineSegment(2, 2, 6, 4);
-        distance = CreateTransectsAndIntersectionsProcess.getMinimumProjectedDistance(end, start);
+        distance = BaselineDistanceAccumulator.getMinimumProjectedDistance(end, start);
         assertEquals("segments 45 to each other", 2, distance, EPS);
     }
 }
