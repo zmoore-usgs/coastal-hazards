86e7009cc997a9274766501b7d2513d948ceecd7
Jordan Walker <jiwalker@usgs.gov>
2013-02-21 18:02:12 -0600

    Added test for results (doesn't actually test anything, just hopefully doesn't fail)

--- coastal-hazards-wps/src/test/java/gov/usgs/cida/coastalhazards/wps/CreateResultsLayerProcessTest.java
+++ coastal-hazards-wps/src/test/java/gov/usgs/cida/coastalhazards/wps/CreateResultsLayerProcessTest.java
@@ -1,9 +1,11 @@
 package gov.usgs.cida.coastalhazards.wps;
 
+import gov.usgs.cida.coastalhazards.util.FeatureCollectionFromShp;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.net.URL;
 import org.apache.commons.io.IOUtils;
+import org.geotools.data.simple.SimpleFeatureCollection;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -17,10 +19,12 @@ public class CreateResultsLayerProcessTest {
      * Test of execute method, of class CreateResultsLayerProcess.
      */
     @Test
-    @Ignore
+    //@Ignore
     public void testExecute() throws Exception {
         URL resource = CreateResultsLayerProcessTest.class.getClassLoader()
                 .getResource("gov/usgs/cida/coastalhazards/jersey/NewJerseyN_results");
+        URL transects = CreateResultsLayerProcessTest.class.getClassLoader()
+                .getResource("gov/usgs/cida/coastalhazards/jersey/NewJerseyNa_transects.shp");
         BufferedReader reader = new BufferedReader(new FileReader(resource.getFile()));
         StringBuffer buffer = new StringBuffer();
         String line = null;
@@ -29,10 +33,11 @@ public class CreateResultsLayerProcessTest {
             buffer.append("\n");
         }
         IOUtils.closeQuietly(reader);
-        
+        SimpleFeatureCollection transectfc = (SimpleFeatureCollection)
+                FeatureCollectionFromShp.featureCollectionFromShp(transects);
         // need to get the matching transect layer to run against
         CreateResultsLayerProcess createResultsLayerProcess = new CreateResultsLayerProcess(new DummyImportProcess(), new DummyCatalog());
-        createResultsLayerProcess.execute(buffer, null, null, null, null);
+        createResultsLayerProcess.execute(buffer, transectfc, null, null, null);
     }
     
 }
