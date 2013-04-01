7b96495f64078d702866df528e9fbc335aec0ec1
Jordan S Read <jread@usgs.gov>
2013-04-01 12:38:51 -0500

    removed parallels, optimized loop

--- coastal-hazards-n52/src/main/webapp/R/r_scripts/DSAS_stats.R
+++ coastal-hazards-n52/src/main/webapp/R/r_scripts/DSAS_stats.R
@@ -7,15 +7,11 @@
 localRun <- TRUE
 # comment this out for WPS!!!
 if (localRun){
+  Rprof("DSAS_profiler.txt")
   ci <- 0.95
   input <- "testOut.tsv"
   ptm <- proc.time() # for time of process
 }
-
-numCores <- 8
-library(foreach)
-library(doMC)
-registerDoMC(numCores)
 
 if (ci>=1 || ci<=0.5){
   stop("confidence interval argument must be between 0.5 and 1.0 (non-inclusive)")
@@ -43,6 +39,14 @@ nRead <- length(fileLines)
 blockI <- grep("# ", fileLines)
 blckNm <- sub("# ","",fileLines[blockI])
 numBlck<- length(blockI)
+textBlck <- vector(length=numBlck,mode="character")
+
+for (blockNumber in 1:numBlck){
+  if (blockNumber==numBlck) {enI <- nRead-1}
+  else{enI <- blockI[blockNumber+1]-1}
+  stI <- blockI[blockNumber]+1
+  textBlck[blockNumber] <- paste(fileLines[stI:enI],collapse=delim)
+}
 
 calcLRR <- function(dates,dist){
   rate <- dates
@@ -53,7 +57,7 @@ calcLRR <- function(dates,dist){
   
   LRR_rates <- rate*rateConv 
   LCI <- (CI[2]-CI[1])/2 # LCI
-  return(data.frame("LRR"=LRR_rates,"LCI"=LCI))
+  return(c(LRR_rates,LCI))
 }
 
 calcWLR <- function(dates,dist,uncy){
@@ -64,14 +68,14 @@ calcWLR <- function(dates,dist,uncy){
   rate <- coef["rate"]
   WLR_rates <- rate*rateConv 
   WCI  <- (CI[2]-CI[1])/2 # WCI
-  return(data.frame("WLR"=WLR_rates,"WCI"=WCI))
+  return(c(WLR_rates,WCI))
 }
 calcNSM <- function(dates,dist){
   firstDateIdx <- which.min(dates)
   lastDateIdx  <- which.max(dates)
   NSM_dist <- dist[firstDateIdx]-dist[lastDateIdx]
   EPR_rates <- NSM_dist/(as(dates[lastDateIdx]-dates[firstDateIdx],"numeric"))*rateConv
-  return(data.frame("NSM"=NSM_dist,"EPR"=EPR_rates))
+  return(c(NSM_dist,EPR_rates))
 }
 
 LRR <-  rep(NA,numBlck)
@@ -82,11 +86,8 @@ SCE <-  rep(NA,numBlck)
 NSM <-  rep(NA,numBlck)
 EPR <-  rep(NA,numBlck)
 
-getDSAS <- function(blockNumber){   # get indices for start and end of block
-  if (blockNumber==numBlck) {enI <- nRead-1}
-  else{enI <- blockI[blockNumber+1]-1}
-  stI <- blockI[blockNumber]+1
-  splitsTxt <- unlist(strsplit(paste(fileLines[stI:enI],collapse=delim),delim))
+getDSAS <- function(blockText){  
+  splitsTxt <- unlist(strsplit(blockText,delim))
   dates <- as(as.Date(splitsTxt[seq(t_i,length(splitsTxt),3)],format="%Y-%m-%d"),"numeric")
   dist  <- as(splitsTxt[seq(d_i,length(splitsTxt),3)],"numeric")
   uncy  <- as(splitsTxt[seq(u_i,length(splitsTxt),3)],"numeric")
@@ -98,27 +99,35 @@ getDSAS <- function(blockNumber){   # get indices for start and end of block
   uncy  <- uncy[useI]
   
   if (length(dates) >= 3) {
-    SCE   <- data.frame("SCE"=(max(dist)-min(dist)))
-    return(data.frame("transect_ID"=blckNm[blockNumber],calcLRR(dates,dist),
-                      calcWLR(dates,dist,uncy),SCE,calcNSM(dates,dist)))
+    LRRout   <- calcLRR(dates,dist)
+    WLRout   <- calcWLR(dates,dist,uncy)
+    SCE   <- (max(dist)-min(dist))
+    NSMout  <- calcNSM(dates,dist)
+    return(c(LRRout,WLRout,SCE,NSMout))
   }
+  else{return(rep(NA,7))}
   
 }
-statsout <- data.frame("transect_ID"=as.numeric(),"LRR"=as.numeric(),
-                       "LCI"=as.numeric(),"WLR"=as.numeric(),
-                       "WCI"=as.numeric(),"SCE"=as.numeric(),
-                       "NSM"=as.numeric(),"EPR"=as.numeric())
+
+
 for (b in 1:numBlck){
-  DSASstats <- getDSAS(b))
+  DSASstats <- getDSAS(textBlck[b])
+  LRR[b] <- DSASstats[1]
+  LCI[b] <- DSASstats[2]
+  WLR[b] <- DSASstats[3]
+  WCI[b] <- DSASstats[4]
+  SCE[b] <- DSASstats[5]
+  NSM[b] <- DSASstats[6]
+  EPR[b] <- DSASstats[7]
 }
 
-proc.time() -ptm
-statsout <- foreach(b=1:numBlck,.combine='rbind') %do% {
-  getDSAS(b)
-}
+statsout <- data.frame("transect_ID"=blckNm,LRR,LCI,WLR,WCI,SCE,NSM,EPR)
 
-colnames(statsout)<-c('transect_ID','LRR','LCI','WLR','WCI','SCE','NSM','EPR')
-if (localRun){proc.time() -ptm}
+if (localRun){
+  proc.time() -ptm
+  Rprof(NULL)
+  summaryRprof(filename = "DSAS_profiler.txt",chunksize=5000)
+}
 
 # output is an identifier and R variable (WPS identifier). The ouput is the name of the text file
 # wps.out: output, text, output title, tabular output data to append to shapefile;
