3b4311602e4c5c4fe5054bfd74f7926d8b433c11
jordan <jordan@igsarmewwsjiw>
2013-04-11 15:18:36 -0500

    updated this test for NaN issue

--- coastal-hazards-wps/src/test/java/gov/usgs/cida/coastalhazards/wps/CreateResultsLayerProcessTest.java
+++ coastal-hazards-wps/src/test/java/gov/usgs/cida/coastalhazards/wps/CreateResultsLayerProcessTest.java
@@ -3,12 +3,18 @@ package gov.usgs.cida.coastalhazards.wps;
 import gov.usgs.cida.coastalhazards.util.FeatureCollectionFromShp;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import org.apache.commons.io.IOUtils;
 import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
+import org.opengis.feature.simple.SimpleFeature;
 
 /**
  *
@@ -16,14 +22,19 @@ import org.junit.Test;
  */
 public class CreateResultsLayerProcessTest {
     
+    private static File outTest = null;
+    @BeforeClass
+    public static void setupAll() throws IOException {
+        outTest = File.createTempFile("test", ".shp");
+        //outTest.deleteOnExit();
+    }
+    
     /**
      * Test of execute method, of class CreateResultsLayerProcess.
      */
     @Test
     //@Ignore
     public void testExecute() throws Exception {
-        File outTest = File.createTempFile("test", ".shp");
-        outTest.deleteOnExit();
         InputStream resourceAsStream = CreateResultsLayerProcessTest.class.getClassLoader()
                                                .getResourceAsStream("gov/usgs/cida/coastalhazards/jersey/NewJerseyN_results.txt");
         URL transects = CreateResultsLayerProcessTest.class.getClassLoader()
@@ -43,4 +54,37 @@ public class CreateResultsLayerProcessTest {
         createResultsLayerProcess.execute(buffer, transectfc, null, null, null, null);
     }
     
+    @Test
+    public void testReadShapeOut() throws IOException, MalformedURLException {
+        URL results = outTest.toURI().toURL();
+        SimpleFeatureCollection resultfc = (SimpleFeatureCollection)
+                FeatureCollectionFromShp.featureCollectionFromShp(results);
+        SimpleFeatureIterator features = resultfc.features();
+        while (features.hasNext()) {
+            SimpleFeature next = features.next();
+            Double attribute = (Double)next.getAttribute("LRR");
+            System.out.println("val: " + attribute);
+        }
+    }
+    
+    @Test
+    public void testParseDouble() {
+        String regular = "1.0";
+        String uffd = "\ufffd";
+        String uffd_w_space = "    \ufffd";
+        String question = "?";
+        String question_w_space = "    ?";
+        String nan = "   NaN";
+        assertEquals(Double.parseDouble(regular), 1.0, 0.001);
+        assertEquals(Double.parseDouble(nan), Double.NaN, 0.001);
+        //assertEquals(Double.parseDouble(uffd), Double.NaN, 0.001);
+        //assertEquals(Double.parseDouble(uffd_w_space), Double.NaN, 0.001);
+        try {
+            //assertEquals(Double.parseDouble(question), Double.NaN, 0.001);
+            //assertEquals(Double.parseDouble(question_w_space), Double.NaN, 0.001);
+        }
+        catch (NumberFormatException nfe) {
+            // this is ok
+        }
+    }
 }
