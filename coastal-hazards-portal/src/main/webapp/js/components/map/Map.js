bd879ef12aa37303622547657c43e9135c7ff766
isuftin <isuftin@usgs.gov>
2013-05-03 14:40:29 -0500

    Moved zoom control over to the right. Moved base layer sets below zoom control

--- coastal-hazards-portal/src/main/webapp/js/components/map/Map.js
+++ coastal-hazards-portal/src/main/webapp/js/components/map/Map.js
@@ -52,6 +52,7 @@ var Map = function(args) {
 
 	LOG.debug('Map.js::constructor:Zooming to extent: ' + initialExtent);
 	me.map.zoomToExtent(initialExtent, true);
+	$('.olControlZoom').css('left', $('#' + mapDivId).width() - $('.olControlZoom').width() - 20);
 
 	LOG.debug('Map.js::constructor: Map class initialized.');
 	return $.extend(me, {
