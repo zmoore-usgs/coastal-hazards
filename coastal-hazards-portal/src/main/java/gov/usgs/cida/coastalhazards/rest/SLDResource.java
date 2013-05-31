7f6356087c37ec151100e3297ec37730bee700d4
Jordan Walker <jiwalker@usgs.gov>
2013-05-31 13:59:20 -0500

    Red white color map with example output

--- coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/SLDResource.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/SLDResource.java
@@ -13,6 +13,9 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("sld")
 public class SLDResource {
+    
+    private static final int NUMBER_OF_BINS = 10;
+    private static final int STROKE_WIDTH = 1;
 
     @GET
     @Path("redwhite/{id}/{attr}")
@@ -21,10 +24,15 @@ public class SLDResource {
             @PathParam("id") String id,
             @PathParam("attr") String attr) {
         String result = "";
+        
+        float[] thresholds = makeSteps(0.0f, 100.0f, NUMBER_OF_BINS);
+        String[] colors = makeColors("#FFFFFF", "#FF0000", NUMBER_OF_BINS-1);
+        
         StringBuilder sld = new StringBuilder();
         sld.append("<sld:StyledLayerDescriptor")
             .append(" xmlns=\"http://www.opengis.net/ogc\"")
-            .append(" xmlns:sld=\"http://www.opengis.net/sld\"") 
+            .append(" xmlns:sld=\"http://www.opengis.net/sld\"")
+            .append(" xmlns:ogc=\"http://www.opengis.net/ogc\"")
             .append(" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"")
             .append(" version=\"1.0.0\"")
             .append(" xsi:schemaLocation=\"http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd\">")
@@ -36,16 +44,42 @@ public class SLDResource {
             .append("<sld:UserStyle>")
             .append("<sld:Name>redWhiteMap</sld:Name>")
             .append("<sld:Title>Red to White ColorMap</sld:Title>")
-            .append("<sld:FeatureTypeStyle>") 
-            .append("<sld:Rule>")
+            .append("<sld:FeatureTypeStyle>");
+        
+       for (int i=0; i<NUMBER_OF_BINS-1; i++) {
+            sld.append("<sld:Rule>")
+            .append("<sld:Filter>")
+            .append("<ogc:PropertyIsGreaterThanOrEqualTo>")
+            .append("<ogc:PropertyName>")
+                .append(attr)
+            .append("</ogc:PropertyName>")
+            .append("<ogc:Literal>")
+                .append(thresholds[i])
+            .append("</ogc:Literal>")
+            .append("</ogc:PropertyIsGreaterThanOrEqualTo>")
+            .append("<ogc:PropertyIsLessThan>")
+            .append("<ogc:PropertyName>")
+                .append(attr)
+            .append("</ogc:PropertyName>")
+            .append("<ogc:Literal>")
+                .append(thresholds[i+1])
+            .append("</ogc:Literal>")
+            .append("</ogc:PropertyIsLessThan>")
+            .append("</sld:Filter>")
             .append("<sld:LineSymbolizer>")
-            .append("<sld:ColorMap>")
-            .append("<sld:ColorMapEntry color=\"#FFFFF\" quantity=\"0.0\"/>")
-            .append("<sld:ColorMapEntry color=\"#FF0000\" quantity=\"100.0\"/>")
-            .append("</sld:ColorMap>")
+            .append("<sld:Stroke>")
+            .append("<sld:CssParameter name=\"stroke\">")
+                .append(colors[i])
+            .append("</sld:CssParameter>")
+            .append("<sld:CssParameter name=\"stroke-width\">")
+                .append(STROKE_WIDTH)
+            .append("</sld:CssParameter>")
+            .append("</sld:Stroke>")
             .append("</sld:LineSymbolizer>")
-            .append("</sld:Rule>")
-            .append("</sld:FeatureTypeStyle>") 
+            .append("</sld:Rule>");
+        }
+       
+        sld.append("</sld:FeatureTypeStyle>") 
             .append("</sld:UserStyle>")
             .append("</sld:NamedLayer>")
             .append("</sld:StyledLayerDescriptor>");
@@ -54,24 +88,32 @@ public class SLDResource {
             return result;
     }
     
-    protected static float[] makeSteps(float min, float max, int count) {
+    protected static float[] makeSteps(float start, float end, int count) {
         float[] steps = new float[count];
-        if (min > max) {
-            throw new IllegalArgumentException("Min cannot be greater than max");
-        }
         if (count < 2) {
             throw new IllegalArgumentException("Count must be 2 or more");
         }
-        float range = max - min;
+        // range is negative if end < start
+        float range = end - start;
         float stepSize = range / (count - 1);
         for (int i=0; i<count; i++) {
-            steps[i] = min + (stepSize * i);
+            steps[i] = start + (stepSize * i);
         }
         return steps;
     }
     
     protected static String[] makeColors(String hexStart, String hexEnd, int count) {
-        
+        String[] result = new String[count];
         Color startColor = Color.decode(hexStart);
+        Color endColor = Color.decode(hexEnd);
+        float[] reds = makeSteps((float)startColor.getRed(), (float)endColor.getRed(), count);
+        float[] greens = makeSteps((float)startColor.getGreen(), (float)endColor.getGreen(), count);
+        float[] blues = makeSteps((float)startColor.getBlue(), (float)endColor.getBlue(), count);
+        for (int i=0; i<count; i++) {
+            int rgb = (int)blues[i] | ((int)greens[i] << 8) | ((int)reds[i] << 16);
+            String gradient = String.format("#%06X", (0xFFFFFF & rgb));
+            result[i] = gradient;
+        }
+        return result;
     }
 }
--- coastal-hazards-portal/src/test/java/gov/usgs/cida/coastalhazards/rest/SLDResourceTest.java
+++ coastal-hazards-portal/src/test/java/gov/usgs/cida/coastalhazards/rest/SLDResourceTest.java
@@ -38,4 +38,28 @@ public class SLDResourceTest {
         assertEquals(6.66, steps[2], 0.01);
         assertEquals(10, steps[3], 0.01);
     }
+    
+    @Test
+    public void testMakeColors2() {
+        String[] steps = SLDResource.makeColors("#ffffff", "#ff0000", 2);
+        assertEquals("#FFFFFF", steps[0]);
+        assertEquals("#FF0000", steps[1]);
+    }
+    
+    @Test
+    public void testMakeColors10() {
+        String[] steps = SLDResource.makeColors("#ffffff", "#ff0000", 10);
+        assertEquals("#FFFFFF", steps[0]);
+        assertEquals("#FFE2E2", steps[1]);
+        assertEquals("#FFC6C6", steps[2]);
+        assertEquals("#FFAAAA", steps[3]);
+        assertEquals("#FF8D8D", steps[4]);
+        assertEquals("#FF7171", steps[5]);
+        assertEquals("#FF5555", steps[6]);
+        assertEquals("#FF3838", steps[7]);
+        assertEquals("#FF1C1C", steps[8]);
+        assertEquals("#FF0000", steps[9]);
+    }
+    
+    
 }
