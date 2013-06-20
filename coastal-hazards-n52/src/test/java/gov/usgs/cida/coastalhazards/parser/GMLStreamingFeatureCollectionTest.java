86fa7131b50cd626696de7adcd0da5189a43d065
Jordan Walker <jiwalker@usgs.gov>
2013-06-19 19:30:36 -0500

    I think the feature collection stuff is fixed, needed to get geotools versions in order * have a test now

--- coastal-hazards-n52/src/test/java/gov/usgs/cida/coastalhazards/parser/GMLStreamingFeatureCollectionTest.java
+++ coastal-hazards-n52/src/test/java/gov/usgs/cida/coastalhazards/parser/GMLStreamingFeatureCollectionTest.java
@@ -1,20 +1,64 @@
+
 package gov.usgs.cida.coastalhazards.parser;
 
+import com.vividsolutions.jts.geom.Geometry;
 import java.io.File;
-import java.net.URISyntaxException;
 import java.net.URL;
+import org.geotools.feature.FeatureIterator;
 import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.opengis.feature.Feature;
+import org.opengis.feature.simple.SimpleFeature;
 
 /**
  *
- * @author Jordan Walker <jiwalker@usgs.gov>
+ * @author tkunicki
  */
 public class GMLStreamingFeatureCollectionTest {
+    
+    public GMLStreamingFeatureCollectionTest() {
+    }
 
     @Test
-    public void testParse() throws URISyntaxException {
-        URL gml = getClass().getClassLoader().getResource("FeatureCollectionRParserGML.xml");
-        GMLStreamingFeatureCollection collection = new GMLStreamingFeatureCollection(new File(gml.toURI()));
+    public void testSimpleWFSParse() {
+        File file = getFile();
+        assertThat(file, notNullValue());
+        
+        GMLStreamingFeatureCollection fc = new GMLStreamingFeatureCollection(file);
+        assertThat(fc, notNullValue());
+        assertThat(fc.getBounds(), notNullValue());
+        assertThat(fc.getSchema(), notNullValue());
+        assertThat(fc.size(), equalTo(324));
+        
+        FeatureIterator fi = null;
+        try {
+            fi = fc.features();
+            assertThat(fi, notNullValue());
+            assertThat(fi.hasNext(), is(true));
+            while(fi.hasNext()) {
+                Feature f = fi.next();
+                assertThat(f, instanceOf(SimpleFeature.class));
+                SimpleFeature sf = (SimpleFeature)f;
+                Object go = sf.getDefaultGeometry();
+                assertThat(go, instanceOf(Geometry.class));
+            }
+        } finally {
+            if (fi != null) {
+                fi.close();
+            }
+        }
+        
     }
     
+    private File getFile() {
+        URL url = getClass().getClassLoader().getResource("collection.xml");
+        assertThat(url, notNullValue());
+        assertThat(url.getProtocol(), equalTo("file"));
+        String path = url.getPath();
+        assertThat(path, notNullValue());
+        File file = new File(path);
+        assertThat(file.canRead(), equalTo(true));
+        return file;
+    } 
 }
