9e59f7fcbbf0dfed29ad390757c8ec3e58ca669b
Jordan Walker <jiwalker@usgs.gov>
2013-06-25 17:11:28 -0500

    changing sld over to jsp for cleaner xml building * other slds we host can use this pattern

--- coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/SLDResource.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/SLDResource.java
@@ -1,11 +1,13 @@
 package gov.usgs.cida.coastalhazards.rest.data;
 
+import com.sun.jersey.api.view.Viewable;
 import java.awt.Color;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -14,82 +16,24 @@ import javax.ws.rs.core.MediaType;
 @Path("sld")
 public class SLDResource {
 
-	private static final int NUMBER_OF_BINS = 10;
-	private static final int STROKE_WIDTH = 3;
-
 	@GET
 	@Path("redwhite/{id}/{attr}")
 	@Produces(MediaType.APPLICATION_XML)
-	public String getRedWhiteSLD(
+	public Response getRedWhiteSLD(
 			@PathParam("id") String id,
 			@PathParam("attr") String attr) {
 
-		float[] thresholds = makeSteps(0.0f, 100.1f, NUMBER_OF_BINS + 1);
-		String[] colors = makeColors("#FFFFFF", "#FF0000", NUMBER_OF_BINS);
-
-		StringBuilder sld = new StringBuilder();
-		sld.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
-				.append("<sld:StyledLayerDescriptor")
-				.append(" xmlns=\"http://www.opengis.net/ogc\"")
-				.append(" xmlns:sld=\"http://www.opengis.net/sld\"")
-				.append(" xmlns:ogc=\"http://www.opengis.net/ogc\"")
-				.append(" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"")
-				.append(" xmlns:xlink=\"http://www.w3.org/1999/xlink\"")
-				.append(" version=\"1.0.0\"")
-				.append(" xsi:schemaLocation=\"http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd\">")
-				.append("<sld:NamedLayer>")
-				.append("<sld:Name>")
-				.append(id)
-				.append("</sld:Name>")
-				.append("<sld:UserStyle>")
-				.append("<sld:Name>redwhite</sld:Name>")
-				.append("<sld:Title>Red to White ColorMap</sld:Title>")
-				.append("<sld:FeatureTypeStyle>");
-
-		for (int i = 0; i < NUMBER_OF_BINS; i++) {
-			sld.append("<sld:Rule>")
-					.append("<ogc:Filter>")
-					.append("<ogc:And>")
-					.append("<ogc:PropertyIsGreaterThanOrEqualTo>")
-					.append("<ogc:PropertyName>")
-					.append(attr)
-					.append("</ogc:PropertyName>")
-					.append("<ogc:Literal>")
-					.append(thresholds[i])
-					.append("</ogc:Literal>")
-					.append("</ogc:PropertyIsGreaterThanOrEqualTo>")
-					.append("<ogc:PropertyIsLessThan>")
-					.append("<ogc:PropertyName>")
-					.append(attr)
-					.append("</ogc:PropertyName>")
-					.append("<ogc:Literal>")
-					.append(thresholds[i + 1])
-					.append("</ogc:Literal>")
-					.append("</ogc:PropertyIsLessThan>")
-					.append("</ogc:And>")
-					.append("</ogc:Filter>")
-					.append("<sld:LineSymbolizer>")
-					.append("<sld:Stroke>")
-					.append("<sld:CssParameter name=\"stroke\">")
-					.append(colors[i])
-					.append("</sld:CssParameter>")
-					.append("<sld:CssParameter name=\"stroke-width\">")
-					.append(STROKE_WIDTH)
-					.append("</sld:CssParameter>")
-					.append("<sld:CssParameter name=\"stroke-opacity\">")
-					.append(1)
-					.append("</sld:CssParameter>")
-					.append("</sld:Stroke>")
-					.append("</sld:LineSymbolizer>")
-					.append("</sld:Rule>");
-		}
-
-		sld.append("</sld:FeatureTypeStyle>")
-				.append("</sld:UserStyle>")
-				.append("</sld:NamedLayer>")
-				.append("</sld:StyledLayerDescriptor>");
-
-		return sld.toString();
+        RedWhiteParams sldParams = new RedWhiteParams();
+        
+		float[] thresholds = makeSteps(0.0f, 100.1f, sldParams.getNUMBER_OF_BINS() + 1);
+		String[] colors = makeColors("#FFFFFF", "#FF0000", sldParams.getNUMBER_OF_BINS());
+        
+		sldParams.setId(id);
+        sldParams.setAttr(attr);
+        sldParams.setThresholds(thresholds);
+        sldParams.setColors(colors);
+        
+		return Response.ok(new Viewable("/redwhite.jsp", sldParams)).build();
 	}
 
 	protected static float[] makeSteps(float start, float end, int count) {
