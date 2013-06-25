222f86787d8f7379439718bd0239b229d6c9c0d1
Jordan Walker <jiwalker@usgs.gov>
2013-06-25 15:32:53 -0500

    fixed imports on SLD test

--- coastal-hazards-portal/src/test/java/gov/usgs/cida/coastalhazards/rest/data/SLDResourceTest.java
+++ coastal-hazards-portal/src/test/java/gov/usgs/cida/coastalhazards/rest/data/SLDResourceTest.java
@@ -1,14 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gov.usgs.cida.coastalhazards.rest.data;
 
-import gov.usgs.cida.coastalhazards.rest.data.SLDResource;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
