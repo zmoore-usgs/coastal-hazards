5b0ab4bcf47ebe68f50d738d6c3739684b499434
Jordan S Read <jread@usgs.gov>
2013-06-26 15:47:17 -0500

    cleaning up, using RData

--- coastal-hazards-n52/src/main/webapp/R/r_scripts/R/storm.service.R
+++ coastal-hazards-n52/src/main/webapp/R/r_scripts/R/storm.service.R
@@ -1,25 +1,12 @@
 
 storm.service = function(serviceEndpoint,attribute){
 	subType	<-	attribute
-	supportSubTypes <- c('PCOL1','PCOL2','PCOL3','PCOL4','PCOL5',
-	                     'POVW1','POVW2','POVW3','POVW4','POVW5',
-	                     'PIND1','PIND2','PIND3','PIND4','PIND5') # more to come...etc...
-
+	load("data/item.data.RData")
+	
 	if (!any(grepl(subType,supportSubTypes))){
 		stop(c(subType,' is not supported for this type'))
 		}
-
-	# type definitions:
-	typeDef <- list('POVW'='Overwash occurs when sand is transported landward over the beach and dune by waves and surge',
-	                'PIND'='Inundation occurs when the beach and dune are completely and continuously submerged by surge and wave setup',
-	                'PCOL'='Collision occurs when the dune toe is eroded by waves and surge')
-
-	# need additional synonyms------
-	synDataSrc <- list('lidar'=c('lidar','EAARL Topography'),
-	                   'aerial photographs'=c('air photos','aerial photographs'),
-	                   'NOAA SLOSH model'=c('SLOSH','Storm Surge Maximum of the Maximum'),
-	                   'SWAN model'=c('Simulating WAves Nearshore','SWAN'),
-	                   'coastal survey maps'=c('T-sheets','coastal survey maps'))        
+      
 	subTypeDataSrc <- names(synDataSrc)
 
 	doc <- xmlInternalTreeParse(serviceEndpoint)
