zmoore-usgs/coastal-hazards

commit 9cce09fce2b8f175dec658eea9dcf5d16fd9ae15

squiggle needs to be requested slightly differently with newer n52

coastal-hazards-ui/src/main/java/gov/usgs/cida/coastalhazards/service/ExportService.java
@@ -41,6 +41,7 @@ public class ExportService extends HttpServlet {
         String filename = request.getParameter("filename");
         String data = request.getParameter("data");
         String type = request.getParameter("type");
+        String encoding = "base64"; // hardcode this for now
         String shortName = request.getParameter("shortName");
         String url = request.getRequestURL().toString();
 
@@ -89,7 +90,7 @@ public class ExportService extends HttpServlet {
                     + "</wps:Input>"
                     + "</wps:DataInputs>"
                     + "<wps:ResponseForm>"
-                    + "<wps:RawDataOutput>"
+                    + "<wps:RawDataOutput mimeType=\"" + type + "\" encoding=\"" + encoding + "\">"
                     + "<ows:Identifier>output</ows:Identifier>"
                     + "</wps:RawDataOutput>"
                     + "</wps:ResponseForm>"
@@ -122,7 +123,7 @@ public class ExportService extends HttpServlet {
 
         StringBuilder sb = new StringBuilder(data);
         byte[] dataByteArr;
-        if ("image/png;base64".equalsIgnoreCase(type) || "image/tiff;base64".equalsIgnoreCase(type)) {
+        if ("base64".equalsIgnoreCase(encoding)) {
             dataByteArr = new BASE64Decoder().decodeBuffer(data.toString());
         } else {
             dataByteArr = data.toString().getBytes("UTF-8");

coastal-hazards-ui/src/main/webapp/js/stages/results.js
@@ -858,7 +858,7 @@ var Results = {
                 $('<input />').attr({
                     'type' : 'hidden',
                     'name' : 'type'
-                }).val('image/png;base64')).
+                }).val('image/png')).
             append(
                 $('<input />').attr({
                     'type' : 'hidden',

commit 0c7a6af0f98aa6d528897740f1c6c69dfd91bbdf

Removing an issue introduced with commit 9cce09fce2b8f175dec658eea9dcf5d16fd9ae15 -- session export was being exported in Base64

coastal-hazards-ui/src/main/java/gov/usgs/cida/coastalhazards/service/ExportService.java
@@ -41,7 +41,7 @@ public class ExportService extends HttpServlet {
         String filename = request.getParameter("filename");
         String data = request.getParameter("data");
         String type = request.getParameter("type");
-        String encoding = "base64"; // hardcode this for now
+        String encoding = StringUtils.isNotBlank(request.getParameter("encoding")) && request.getParameter("encoding").toLowerCase().equals("utf-8") ? request.getParameter("encoding") : "base64";
         String shortName = request.getParameter("shortName");
         String url = request.getRequestURL().toString();
 
@@ -126,7 +126,7 @@ public class ExportService extends HttpServlet {
         if ("base64".equalsIgnoreCase(encoding)) {
             dataByteArr = new BASE64Decoder().decodeBuffer(data.toString());
         } else {
-            dataByteArr = data.toString().getBytes("UTF-8");
+            dataByteArr = data.getBytes(encoding);
         }
         int length = dataByteArr.length;
 
@@ -138,7 +138,7 @@ public class ExportService extends HttpServlet {
             response.setHeader("Content-Disposition", "attachment;filename=" + filename);
             response.setHeader("Pragma", "no-cache");
             response.setHeader("Expires", "0");
-
+			
             in = new ByteArrayInputStream(dataByteArr);
             out = response.getOutputStream();
             IOUtils.copy(in, out);

coastal-hazards-ui/src/main/webapp/js/session/session.js
@@ -603,12 +603,17 @@ var Session = function(name, isPerm) {
                 'type' : 'hidden',
                 'name' : 'filename'
             }).val('cch_session_' + me.getCurrentSessionKey() + '.json')).
+					append(
+            $('<input />').attr({
+                'type' : 'hidden',
+                'name' : 'encoding'
+            }).val('UTF-8')).
                 append(
             $('<input />').attr({
                 'type' : 'hidden',
                 'name' : 'data'
-            }).val(localStorage['coastal-hazards']))
-            $('body').append(exportForm)
+            }).val(localStorage['coastal-hazards']));
+            $('body').append(exportForm);
             exportForm.attr('action', 'service/export');
             exportForm.submit();
             exportForm.remove();