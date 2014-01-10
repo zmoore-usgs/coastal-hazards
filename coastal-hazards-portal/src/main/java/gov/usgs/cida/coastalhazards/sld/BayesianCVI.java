c758004c18b2204eb7d322c5ed1d687e5d0076c5
Ivan Suftin <isuftin@usgs.gov>
2014-01-10 09:55:10 -0600

    Added a slash in front of the path for SLDs so Jersey can pick them up correctly

--- coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/sld/BayesianCVI.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/sld/BayesianCVI.java
@@ -15,7 +15,7 @@ public final class BayesianCVI {
     protected static final float[] thresholds = {1.0f, 10.0f, 33.0f, 66.0f, 90.0f, 99.0f};
     protected static final String[] colors = {"#005CE6", "#00A884", "#55FF00", "#FFFF00", "#FFAA00", "#FF0000", "#734C00"};
 	
-	protected static final String jspPath = "SLD/bins_point.jsp";
+	protected static final String jspPath = "/SLD/bins_point.jsp";
 	protected static final String units = "%";
 	protected static final List<Map<String,Object>> bins;
 	static {
