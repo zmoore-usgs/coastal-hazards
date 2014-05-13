652e39c002d4c8c6e7c8fd00b046181577e54c22
Ivan Suftin <isuftin@usgs.gov>
2014-05-13 14:08:52 -0500

    Formatting

--- coastal-hazards-portal/src/main/webapp/js/application/front/OnReady.js
+++ coastal-hazards-portal/src/main/webapp/js/application/front/OnReady.js
@@ -7,53 +7,53 @@
 /*global LOG*/
 /*global OpenLayers*/
 $(document).ready(function () {
-    "use strict";
+	"use strict";
 
-    splashUpdate("Initializing Logging...");
-    initializeLogging({
-        LOG4JS_LOG_THRESHOLD: CCH.CONFIG.development ? 'debug' : 'info'
-    });
-    CCH.LOG = LOG;
+	splashUpdate("Initializing Logging...");
+	initializeLogging({
+		LOG4JS_LOG_THRESHOLD: CCH.CONFIG.development ? 'debug' : 'info'
+	});
+	CCH.LOG = LOG;
 
-    $(document).ajaxStart(function () {
-        $('body').css('cursor', 'wait');
-    });
-    $(document).ajaxStop(function () {
-        $('body').css('cursor', 'default');
-    });
-    
-    splashUpdate("Initializing Session Subsystem...");
-    CCH.session = new CCH.Objects.Session();
+	$(document).ajaxStart(function () {
+		$('body').css('cursor', 'wait');
+	});
+	$(document).ajaxStop(function () {
+		$('body').css('cursor', 'default');
+	});
 
-    splashUpdate("Initializing Map...");
-    CCH.map = new CCH.Objects.Front.Map({
-        mapDiv: 'map'
-    }).init();
+	splashUpdate("Initializing Session Subsystem...");
+	CCH.session = new CCH.Objects.Session();
 
-    splashUpdate("Initializing OWS Services...");
-    CCH.ows = new CCH.Util.OWS().init();
+	splashUpdate("Initializing Map...");
+	CCH.map = new CCH.Objects.Front.Map({
+		mapDiv: 'map'
+	}).init();
 
-    splashUpdate("Initializing Items...");
-    CCH.items = new CCH.Objects.Items();
+	splashUpdate("Initializing OWS Services...");
+	CCH.ows = new CCH.Util.OWS().init();
 
-    splashUpdate("Initializing UI...");
-    CCH.ui = CCH.Objects.Front.UI({
-        applicationOverlayId: 'application-overlay',
-        headerRowId: 'header-row',
-        footerRowId: 'footer-row',
-        contentRowId: 'content-row',
-        mapdivId: 'map',
-        shareModalId: 'modal-content-share',
-        shareUrlButtonId: 'modal-share-summary-url-button',
-        shareInputId: 'modal-share-summary-url-inputbox',
-        shareTwitterBtnId: 'multi-card-twitter-button',
-        helpModalId: 'helpModal',
-        helpModalBodyId: 'help-modal-body',
-        slideContainerDivId: 'application-slide-items-content-container',
-        slideItemsContainerId: 'application-slide-items-container',
-        slideBucketContainerId: 'application-slide-bucket-container',
-        slideSearchContainerId: 'application-slide-search-container'
-    });
-    
-    $(window).trigger('cch.app.initialized');
+	splashUpdate("Initializing Items...");
+	CCH.items = new CCH.Objects.Items();
+
+	splashUpdate("Initializing UI...");
+	CCH.ui = CCH.Objects.Front.UI({
+		applicationOverlayId: 'application-overlay',
+		headerRowId: 'header-row',
+		footerRowId: 'footer-row',
+		contentRowId: 'content-row',
+		mapdivId: 'map',
+		shareModalId: 'modal-content-share',
+		shareUrlButtonId: 'modal-share-summary-url-button',
+		shareInputId: 'modal-share-summary-url-inputbox',
+		shareTwitterBtnId: 'multi-card-twitter-button',
+		helpModalId: 'helpModal',
+		helpModalBodyId: 'help-modal-body',
+		slideContainerDivId: 'application-slide-items-content-container',
+		slideItemsContainerId: 'application-slide-items-container',
+		slideBucketContainerId: 'application-slide-bucket-container',
+		slideSearchContainerId: 'application-slide-search-container'
+	});
+
+	$(window).trigger('cch.app.initialized');
 });
