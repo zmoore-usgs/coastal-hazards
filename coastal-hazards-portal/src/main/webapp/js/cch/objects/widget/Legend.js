dd81e0f5e6dc0256df3df227e2954de2c332457b
Ivan Suftin <isuftin@usgs.gov>
2014-05-20 11:48:58 -0500

    Update for legends with missing tables

--- coastal-hazards-portal/src/main/webapp/js/cch/objects/widget/Legend.js
+++ coastal-hazards-portal/src/main/webapp/js/cch/objects/widget/Legend.js
@@ -539,6 +539,8 @@ CCH.Objects.Widget.Legend = function (args) {
 			// There are no more legends to be built, filter and add the legend to the document
 			legendTables = legendTables.unique(function (table) {
 				return $(table).attr('legend-attribute');
+			}).filter(function (table) { // Remove any array items that are -1
+				return table !== -1;
 			});
 
 			if (legendTables.length === 1) {
