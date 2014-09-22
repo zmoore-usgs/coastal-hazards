69bc7b4926e4d7e188468dd698a7d30a9b9563cd
Suftin, Ivan <isuftin@usgs.gov>
2014-09-22 10:28:55 -0500

    Removed GET request handling on ShapefileImportService

--- coastal-hazards-ui/src/main/java/gov/usgs/cida/coastalhazards/service/ShapefileImportService.java
+++ coastal-hazards-ui/src/main/java/gov/usgs/cida/coastalhazards/service/ShapefileImportService.java
@@ -265,12 +265,6 @@ public class ShapefileImportService extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
