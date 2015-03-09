e57e8f14f378caa106fc4e8196761267e4019856
Jordan Walker <jiwalker@usgs.gov>
2015-03-09 18:08:36 -0500

    Moving stuff around to get the template workflow worked out

--- coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/LayerResource.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/LayerResource.java
@@ -1,6 +1,7 @@
 package gov.usgs.cida.coastalhazards.rest.data;
 
 import gov.usgs.cida.coastalhazards.exception.PreconditionFailedException;
+import gov.usgs.cida.coastalhazards.gson.GsonUtil;
 import gov.usgs.cida.coastalhazards.model.Service;
 import gov.usgs.cida.coastalhazards.rest.data.util.GeoserverUtil;
 import gov.usgs.cida.coastalhazards.rest.security.CoastalHazardsTokenBasedSecurityFilter;
@@ -10,15 +11,20 @@ import gov.usgs.cida.utilities.properties.JNDISingleton;
 import it.geosolutions.geoserver.rest.GeoServerRESTPublisher;
 import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.apache.commons.lang.StringUtils;
 
@@ -45,17 +51,33 @@ public class LayerResource {
 
 	@POST
 	@Path("/")
-	@RolesAllowed({CoastalHazardsTokenBasedSecurityFilter.CCH_ADMIN_ROLE})
+	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
+	@Produces(MediaType.APPLICATION_JSON)
+	//@RolesAllowed({CoastalHazardsTokenBasedSecurityFilter.CCH_ADMIN_ROLE})
 	public Response createLayer(@Context HttpServletRequest req, InputStream postBody) {
-		String newId = IdGenerator.generate();
-		List<Service> added = GeoserverUtil.addLayer(postBody, newId);
+		Response response = null;
+		Map<String, String> responseMap = new HashMap<>();
 		
-		return null;
+		String newId = IdGenerator.generate();
+		// TODO metadata is ignored for now, would need to pull metadata out of zip
+		List<Service> added = GeoserverUtil.addLayer(postBody, newId);
+		if (!added.isEmpty()) {
+			// just get the first (WFS) until we add to database TODO
+			Service service = added.get(0);
+			String endpoint = service.getEndpoint();
+			String serviceParameter = service.getServiceParameter();
+			responseMap.put("endpoint", endpoint);
+			responseMap.put("serviceParameter", serviceParameter);
+			response = Response.ok(GsonUtil.getDefault().toJson(responseMap, HashMap.class), MediaType.APPLICATION_JSON_TYPE).build();
+		} else {
+			response = Response.serverError().entity("Unable to create layer").build();
+		}
+		return response;
 	}
 	
-	@RolesAllowed({CoastalHazardsTokenBasedSecurityFilter.CCH_ADMIN_ROLE})
 	@DELETE
 	@Path("/{layer}")
+	@RolesAllowed({CoastalHazardsTokenBasedSecurityFilter.CCH_ADMIN_ROLE})
 	public Response deleteLaterFromGeoserver(@Context HttpServletRequest req, @PathParam("layer") String layer) throws URISyntaxException {
 		if (StringUtils.isBlank(layer)) {
 			throw new PreconditionFailedException();
--- coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/TemplateResource.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/TemplateResource.java
@@ -1,13 +1,17 @@
 package gov.usgs.cida.coastalhazards.rest.data;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import gov.usgs.cida.coastalhazards.jpa.ItemManager;
 import gov.usgs.cida.coastalhazards.model.Item;
-import gov.usgs.cida.coastalhazards.model.Service;
-import gov.usgs.cida.coastalhazards.rest.data.util.GeoserverUtil;
+import gov.usgs.cida.coastalhazards.rest.data.util.MetadataUtil;
 import gov.usgs.cida.coastalhazards.rest.security.CoastalHazardsTokenBasedSecurityFilter;
-import gov.usgs.cida.utilities.IdGenerator;
-import java.io.InputStream;
-import java.util.LinkedList;
+import gov.usgs.cida.coastalhazards.util.ogc.CSWService;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Iterator;
 import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
@@ -18,8 +22,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
 
-import static gov.usgs.cida.coastalhazards.rest.data.ItemResource.itemURI;
 
 /**
  *
@@ -30,31 +35,69 @@ public class TemplateResource {
 	
 	@POST
 	@Path("/item/{id}")
-	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@RolesAllowed({CoastalHazardsTokenBasedSecurityFilter.CCH_ADMIN_ROLE})
-	public Response instantiateTemplate(@Context HttpServletRequest request, @PathParam("id") String id, InputStream postBody) {
+	public Response instantiateTemplate(@Context HttpServletRequest request, @PathParam("id") String id, String content) {
 		Response response = null;
 		try (ItemManager manager = new ItemManager()) {
 			Item template = manager.load(id);
 			if (template.getItemType() != Item.ItemType.template) {
 				throw new UnsupportedOperationException("Only template items may be instantiated");
 			}
-			String newId = IdGenerator.generate();
-			List<Service> added = GeoserverUtil.addLayer(postBody, newId);
-			if (added.size() > 0) {
-				Item newItem = template.instantiateTemplate(added);
-				manager.persist(newItem);
 
-				List<Item> instances = template.getChildren();
-				if (instances == null) {
-					instances = new LinkedList<>();
+			List<String> childItems = template.proxiedChildren();
+			CSWService metadata = template.fetchCswService();
+			
+			JsonParser parser = new JsonParser();
+			JsonObject parsed = parser.parse(content).getAsJsonObject();
+			// TODO only supporting one level for now, bring in aggs later
+			JsonArray children = parsed.get("children").getAsJsonArray();
+			Iterator<JsonElement> iterator = children.iterator();
+			while (iterator.hasNext()) {
+				JsonObject child = iterator.next().getAsJsonObject();
+				JsonElement childId = child.get("id");
+				String attr = child.get("attr").getAsString();
+				JsonElement serviceId = child.get("serviceId");
+				
+				String summaryJson = MetadataUtil.getSummaryFromWPS(metadata.getEndpoint(), attr);
+				
+				if (childId != null) {
+					String replaceId = childId.getAsString();
+					if (childItems.contains(replaceId)) {
+						
+					} else {
+						// bad request
+					}
+				} else {
+					// generate id for attr
+					
 				}
-				instances.add(newItem);
-				template.setChildren(instances);
-				manager.merge(template);
-
-				response = Response.created(itemURI(newItem)).build();
 			}
+//			if (added.size() > 0) {
+//				Item newItem = template.instantiateTemplate(added);
+//				manager.persist(newItem);
+//
+//				List<Item> instances = template.getChildren();
+//				if (instances == null) {
+//					instances = new LinkedList<>();
+//				}
+//				instances.add(newItem);
+//				template.setChildren(instances);
+//				manager.merge(template);
+//
+//				response = Response.created(itemURI(newItem)).build();
+//			}
+		} catch (IOException ex) {
+			// do something with this exception
+		}
+		catch (ParserConfigurationException ex) {
+			
+		}
+		catch (SAXException ex) {
+			
+		}
+		catch (URISyntaxException ex) {
+			
 		}
 		return response;
 	}
