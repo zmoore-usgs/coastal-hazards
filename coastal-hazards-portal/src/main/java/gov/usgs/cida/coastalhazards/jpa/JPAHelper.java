50c49e2526d1458403d263768477946934d7e1d9
Jordan Walker <jiwalker@usgs.gov>
2015-03-17 17:40:52 -0500

    Catching Exceptions and logging, in both cases return null if there is an exception (may result in 404 rather than 500)

--- coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/jpa/JPAHelper.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/jpa/JPAHelper.java
@@ -4,37 +4,45 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Jordan Walker <jiwalker@usgs.gov>
  */
 public class JPAHelper {
+	
+	private static final Logger log = LoggerFactory.getLogger(JPAHelper.class);
 
-    @PersistenceUnit
-    private static EntityManagerFactory emf = null;
+	@PersistenceUnit
+	private static EntityManagerFactory emf = null;
 
-    public static EntityManagerFactory getEntityManagerFactory() {
-        return getEntityManagerFactory("coastalhazards");
-    }
-    
-    public static EntityManagerFactory getEntityManagerFactory(String unit) {
-        if (emf == null) {
-            emf = Persistence.createEntityManagerFactory(unit);
-        }
-        return emf;
-    }
-    
-    public static void closeEntityManagerFactory() {
-        if (emf != null && emf.isOpen()) {
-            emf.close();
-        }
-    }
-    
-    public static void close(EntityManager em) {
-        if (em != null && em.isOpen()) {
-            em.close();
-        }
-    }
-    
+	public static EntityManagerFactory getEntityManagerFactory() {
+		return getEntityManagerFactory("coastalhazards");
+	}
+
+	public static EntityManagerFactory getEntityManagerFactory(String unit) {
+		if (emf == null) {
+			try {
+				emf = Persistence.createEntityManagerFactory(unit);
+			} catch (Exception e) {
+				log.error("Unable to build entity manager factory", e);
+			}
+		}
+		return emf;
+	}
+
+	public static void closeEntityManagerFactory() {
+		if (emf != null && emf.isOpen()) {
+			emf.close();
+		}
+	}
+
+	public static void close(EntityManager em) {
+		if (em != null && em.isOpen()) {
+			em.close();
+		}
+	}
+
 }
