e667735d465d6f0a039b878349981414c583003f
Jordan Walker <jiwalker@usgs.gov>
2015-03-18 19:23:39 -0500

    Changing id generator to handle bursts of generation.

--- coastal-hazards-portal/src/main/java/gov/usgs/cida/utilities/IdGenerator.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/utilities/IdGenerator.java
@@ -1,29 +1,45 @@
 package gov.usgs.cida.utilities;
 
 import java.util.Date;
+import java.util.Random;
 
 /**
  * Limitation is that it uses current time (in milliseconds) to get the id This
  * is not good for lots of items generated quickly, but should be alright for
  * slow creation
  *
+ * I'm adding an incremental bit to this so that ids generated within close
+ * time frame will not end up with the same id, "warning" this will not be great
+ * for distributed loads as it depends on synchronization.
+ * 
  * @author Jordan Walker <jiwalker@usgs.gov>
  */
 public class IdGenerator {
 
 	private static final String alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
 	private static final int size = alphabet.length();
+	private static int index = new Random().nextInt(size);
 
 	public static String generate() {
 		long time = new Date().getTime();
+		String head = stringify(time);
+		return head + nextTail();
+	}
+	
+	private static String stringify(long sequence) {
 		StringBuilder str = new StringBuilder();
-		while (time > size) {
-			long div = (long) (time / size);
-			int mod = (int) (time % size);
-			time = div;
+		while (sequence > size) {
+			long div = (long) (sequence / size);
+			int mod = (int) (sequence % size);
+			sequence = div;
 			str.append(alphabet.charAt(mod));
 		}
-		str.append(alphabet.charAt((int) time));
+		str.append(alphabet.charAt((int) sequence));
 		return str.reverse().toString();
+	}
+	
+	private static synchronized char nextTail() {
+		index = (index + 1) % size;
+		return alphabet.charAt(index);
 	}
 }
--- coastal-hazards-portal/src/test/java/gov/usgs/cida/utilities/IdGeneratorTest.java
+++ coastal-hazards-portal/src/test/java/gov/usgs/cida/utilities/IdGeneratorTest.java
@@ -1,6 +1,10 @@
 package gov.usgs.cida.utilities;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -8,15 +12,25 @@ import static org.junit.Assert.*;
  * @author Jordan Walker <jiwalker@usgs.gov>
  */
 public class IdGeneratorTest {
-   
-    /**
-     * Test of generate method, of class IdGenerator.
-     */
-    @Test
-    public void testGenerate() throws InterruptedException {
-        String a = IdGenerator.generate();
-        Thread.sleep(2);
-        String b = IdGenerator.generate();
-        assertNotEquals(a, b);
-    }
+
+	/**
+	 * Test of generate method, of class IdGenerator.
+	 */
+	@Test
+	public void testGenerate() throws InterruptedException {
+		String a = IdGenerator.generate();
+		Thread.sleep(2);
+		String b = IdGenerator.generate();
+		assertNotEquals(a, b);
+	}
+
+	@Test
+	public void testQuickGenerate() {
+		Set<String> ids = new HashSet<>();
+		// Currently 58 is the limit within the same millisecond
+		for (int i=0; i<58; i++) {
+			String id = IdGenerator.generate();
+			assertThat(ids.add(id), is(true));
+		}
+	}
 }
