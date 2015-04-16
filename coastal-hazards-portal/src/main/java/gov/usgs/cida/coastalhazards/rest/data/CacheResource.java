e511d2b8bdacf6143adb30e3e411285a7c1448a9
Jordan Walker <jiwalker@usgs.gov>
2015-04-16 15:17:45 -0500

    Adding cache clear mechanism

---
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/CacheResource.java
@@ -0,0 +1,77 @@
+package gov.usgs.cida.coastalhazards.rest.data;
+
+import gov.usgs.cida.coastalhazards.rest.security.CoastalHazardsTokenBasedSecurityFilter;
+import gov.usgs.cida.config.DynamicReadOnlyProperties;
+import gov.usgs.cida.utilities.properties.JNDISingleton;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.config.DiskStoreConfiguration;
+import net.sf.ehcache.config.PersistenceConfiguration;
+import net.sf.ehcache.store.MemoryStoreEvictionPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Jordan Walker <jiwalker@usgs.gov>
+ */
+@Path(DataURI.CACHE_PATH)
+@PermitAll
+public class CacheResource {
+	
+	private static final Logger log = LoggerFactory.getLogger(CacheResource.class);
+
+	private static final String CACHE_NAME_PROP = "coastal-hazards.portal.geoserver.cache.name";
+	private static final String CACHE_NAME = "proxyCache";
+	private static final String CACHE_LOCATION_PROP = "coastal-hazards.portal.geoserver.cache.name";
+	private static final String CACHE_LOCATION = "${java.io.tmpdir}/ehcache";
+	
+	private static final String cacheLocation;
+	private static final String cacheName;
+
+	static {
+		DynamicReadOnlyProperties props = JNDISingleton.getInstance();
+		cacheLocation = props.getProperty(CACHE_LOCATION_PROP, CACHE_LOCATION);
+		cacheName = props.getProperty(CACHE_NAME_PROP, CACHE_NAME);
+	}
+
+	@Path("/")
+	@DELETE
+	@RolesAllowed({CoastalHazardsTokenBasedSecurityFilter.CCH_ADMIN_ROLE})
+	public Response clearCache() {
+		Response response = null;
+		
+		try {
+			CacheConfiguration ehConfig = new CacheConfiguration()
+					.name(cacheName)
+					.memoryStoreEvictionPolicy(MemoryStoreEvictionPolicy.LRU)
+					.persistence(new PersistenceConfiguration()
+							.strategy(PersistenceConfiguration.Strategy.LOCALTEMPSWAP))
+					.clearOnFlush(true);
+			Configuration managerConfig = new Configuration()
+					.diskStore(new DiskStoreConfiguration().path(cacheLocation))
+					.dynamicConfig(false)
+					.cache(ehConfig);
+			CacheManager cacheManager = CacheManager.create(managerConfig);
+			Ehcache ehcache = cacheManager.getEhcache(cacheName);
+			if (ehcache != null) {
+				ehcache.flush();
+				ehcache.removeAll();
+				response = Response.ok().build();
+			} else {
+				response = Response.serverError().entity("Error location cache to clear").build();
+			}
+		} catch (Exception e) {
+			log.error("Unable to clear cache", e);
+			response = Response.serverError().entity("Error location cache to clear").build();
+		}
+		return response;
+	}
+}
