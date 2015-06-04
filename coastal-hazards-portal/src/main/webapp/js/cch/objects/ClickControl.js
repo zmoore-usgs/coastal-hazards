bea6031dbe3f285ac850d859caa41884d9a51c4e
Ivan Suftin <isuftin@usgs.gov>
2015-06-04 10:38:39 -0500

    Simplified setting the markers layer layer index

--- coastal-hazards-portal/src/main/webapp/js/cch/objects/ClickControl.js
+++ coastal-hazards-portal/src/main/webapp/js/cch/objects/ClickControl.js
@@ -19,18 +19,12 @@ CCH.Objects.ClickControl = OpenLayers.Class(OpenLayers.Control, {
 	 */
 	onAddLayer: function () {
 		"use strict";
-		// Whenever a layer is added, it may be added on top of the marker layer. I need to move the marker layer
-		// above all other layers
+		// Whenever a layer is added, it may be added on top of the marker layer. 
+		// I need to move the marker layer above all other layers
 		var baseLayers = this.getLayersBy('isBaseLayer', true),
-			markersLayer = this.getLayersByName('Markers')[0],
-			highestLayer = baseLayers.max(function (layer) {
-				return layer.map.getLayerIndex(layer);
-			}),
-			highestLayerIndex = this.getLayerIndex(highestLayer);
-
-		if (highestLayerIndex !== -1) {
-			this.setLayerIndex(markersLayer, highestLayerIndex + 1);
-		}
+				markersLayer = this.getLayersByName('Markers')[0];
+		
+		this.setLayerIndex(markersLayer, baseLayers.length);
 	},
 	/**
 	 * Handles the event of a click to identify response from the back-end
