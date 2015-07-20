7cf285fc235dab2224855cf9b9bfd1ca62c18ec9
Ivan Suftin <isuftin@usgs.gov>
2015-07-20 08:32:35 -0500

    Reworked tree saving functionality
    Now allows for displayedChildren updating and cleaned up the workflow a bit

--- coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/TreeResource.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/TreeResource.java
@@ -16,10 +16,13 @@ import gov.usgs.cida.coastalhazards.model.Item;
 import gov.usgs.cida.coastalhazards.model.util.Status;
 import gov.usgs.cida.coastalhazards.rest.security.CoastalHazardsTokenBasedSecurityFilter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +37,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -116,45 +120,26 @@ public class TreeResource {
 	@Path("/item/{id}")
 	@RolesAllowed({CoastalHazardsTokenBasedSecurityFilter.CCH_ADMIN_ROLE})
 	public Response updateChildren(@Context HttpServletRequest request, @PathParam("id") String id, String content) {
-		Response response = null;
-		JsonParser parser = new JsonParser();
-		JsonElement parsed = null;
-		try {
-			parsed = parser.parse(content);
-			JsonObject jsonObj = (JsonObject) parsed;
+		Response response;
+		if (StringUtils.isNotBlank(id) && StringUtils.isNotBlank(content)) {
+			try {
+				JsonObject jsonObj = (JsonObject) new JsonParser().parse(content);
+				Map<String, JsonObject> itemMap = new HashMap<>(1);
+				itemMap.put(id, jsonObj);
 
-			if (jsonObj.has("children")) {
-				JsonArray childrenArray = (JsonArray) jsonObj.get("children");
-
-				try (ItemManager manager = new ItemManager()) {
-					Item item = manager.load(id);
-					List<Item> children = new LinkedList<>();
-					Iterator<JsonElement> iterator = childrenArray.iterator();
-					while (iterator.hasNext()) {
-						String childId = iterator.next().getAsString();
-						Item child = manager.load(childId);
-						children.add(child);
-					}
-					item.setChildren(children);
-					manager.merge(item);
-
-					try (StatusManager statusMan = new StatusManager(); ThumbnailManager thumbMan = new ThumbnailManager()) {
-						Status status = new Status();
-						status.setStatusName(Status.StatusName.STRUCTURE_UPDATE);
-						statusMan.save(status);
-
-						thumbMan.updateDirtyBits(id);
-					}
+				if (updateItemChildren(itemMap)) {
+					response = Response.ok().build();
+				} else {
+					response = Response.serverError().build();
 				}
-				response = Response.ok().build();
 
-			} else {
-				log.error("Incoming JSON Object {} has no children", content);
-				throw new BadRequestException();
+			} catch (JsonSyntaxException ex) {
+				log.error("Could not parse incoming content: {}", content, ex);
+				response = Response.status(Response.Status.BAD_REQUEST).build();
 			}
-		} catch (JsonSyntaxException ex) {
-			log.error("Could not parse incoming content: {}", content, ex);
-			throw new BadRequestException();
+		} else {
+			log.error("Incoming ID or content was empty");
+			response = Response.status(Response.Status.BAD_REQUEST).build();
 		}
 
 		return response;
@@ -164,93 +149,62 @@ public class TreeResource {
 	@Path("/item")
 	@RolesAllowed({CoastalHazardsTokenBasedSecurityFilter.CCH_ADMIN_ROLE})
 	public Response updateChildrenBulk(@Context HttpServletRequest request, String content) {
-		Response response = null;
-		JsonParser parser = new JsonParser();
-		JsonElement parsed = parser.parse(content);
-		try {
-			parsed = parser.parse(content);
-			JsonObject jsonObj = (JsonObject) parsed;
-
-			if (jsonObj.has("children")) {
-				try (ItemManager manager = new ItemManager()) {
-					List<Item> itemList = new LinkedList<>();
-					for (Entry<String, JsonElement> entry : jsonObj.entrySet()) {
-
-						boolean success = updateItemChildren(entry.getKey(), (JsonObject) entry.getValue());
-
-						Item parentItem = manager.load(entry.getKey());
-						JsonObject obj = (JsonObject) entry.getValue();
-						List<Item> children = new LinkedList<>();
-
-						// Update the item's children
-						if (obj.has("children")) {
-							Iterator<JsonElement> iterator = obj.get("children").getAsJsonArray().iterator();
-							while (iterator.hasNext()) {
-								String childId = iterator.next().getAsString();
-								Item child = manager.load(childId);
-								children.add(child);
-							}
-							parentItem.setChildren(children);
-						}
-
-						// Update the displayedChildren
-						if (obj.has("displayedChildren")) {
-							Iterator<JsonElement> iterator = obj.get("displayedChildren").getAsJsonArray().iterator();
-							List<String> displayedChildren = new ArrayList<>();
-							while (iterator.hasNext()) {
-								String childId = iterator.next().getAsString();
-								displayedChildren.add(childId);
-							}
-							parentItem.setDisplayedChildren(displayedChildren);
-						}
-
-						itemList.add(parentItem);
-
-						try (ThumbnailManager thumbMan = new ThumbnailManager()) {
-							thumbMan.updateDirtyBits(parentItem.getId());
-						}
+		Response response;
+		if (StringUtils.isNotBlank(content)) {
+			try {
+				JsonObject jsonObj = (JsonObject) new JsonParser().parse(content);
+				
+				Set<Entry<String, JsonElement>> entrySet = jsonObj.entrySet();
+				int itemCount = entrySet.size();
+				if (itemCount > 0) {
+					// Create the map that the updating function expects 
+					Map<String, JsonObject> itemMap = new HashMap<>(itemCount);
+					for (Entry<String, JsonElement> entry : entrySet) {
+						itemMap.put(entry.getKey(), (JsonObject) entry.getValue());
 					}
-					manager.mergeAll(itemList);
 
-					try (StatusManager statusMan = new StatusManager()) {
-						Status status = new Status();
-						status.setStatusName(Status.StatusName.STRUCTURE_UPDATE);
-						statusMan.save(status);
+					// Now that I have the map, update the items
+					if (updateItemChildren(itemMap)) {
+						response = Response.ok().build();
+					} else {
+						response = Response.serverError().build();
 					}
+				} else {
+					log.error("Incoming content had no items");
+					response = Response.status(Response.Status.BAD_REQUEST).build();
 				}
-			} else {
-				log.error("Incoming JSON Object {} has no children", content);
-				throw new BadRequestException();
-			}
-		} catch (JsonSyntaxException ex) {
-			log.error("Could not parse incoming content: {}", content, ex);
-			throw new BadRequestException();
-		}
-		if (parsed instanceof JsonObject) {
-			JsonObject jsonObj = (JsonObject) parsed;
 
-			response = Response.ok().build();
+			} catch (JsonSyntaxException ex) {
+				log.error("Could not parse incoming content: {}", content, ex);
+				response = Response.status(Response.Status.BAD_REQUEST).build();
+			}
+		} else {
+			log.error("Incoming content was empty");
+			response = Response.status(Response.Status.BAD_REQUEST).build();
 		}
+
 		return response;
 	}
 
 	/**
+	 * Updates one or more items for children and displayed children
 	 *
-	 * @param itemId
-	 * @param updateData
-	 * @return
+	 * @return whether items were updated in the database or not
 	 */
-	boolean updateItemChildren(String itemId, JsonObject updateData) {
-		boolean success = false;
+	boolean updateItemChildren(Map<String, JsonObject> items) {
+		List<Item> itemList = new LinkedList<>();
+		boolean updated = false;
+		for (Entry<String, JsonObject> entry : items.entrySet()) {
+			String itemId = entry.getKey();
+			JsonObject updateData = entry.getValue();
+			if (updateData.has("children")) {
+				Item parentItem;
+				List<Item> children;
+				try (ItemManager manager = new ItemManager()) {
+					parentItem = manager.load(itemId);
+					children = new LinkedList<>();
 
-		if (updateData.has("children")) {
-			try (ItemManager manager = new ItemManager()) {
-				List<Item> itemList = new LinkedList<>();
-				Item parentItem = manager.load(itemId);
-				List<Item> children = new LinkedList<>();
-
-				// Update the item's children
-				if (updateData.has("children")) {
+					// Update the item's children
 					Iterator<JsonElement> iterator = updateData.get("children").getAsJsonArray().iterator();
 					while (iterator.hasNext()) {
 						String childId = iterator.next().getAsString();
@@ -258,38 +212,61 @@ public class TreeResource {
 						children.add(child);
 					}
 					parentItem.setChildren(children);
-				}
 
-				// Update the displayedChildren
-				if (updateData.has("displayedChildren")) {
-					Iterator<JsonElement> iterator = updateData.get("displayedChildren").getAsJsonArray().iterator();
-					List<String> displayedChildren = new ArrayList<>();
-					while (iterator.hasNext()) {
-						String childId = iterator.next().getAsString();
-						displayedChildren.add(childId);
+					// Update the item's displayedChildren
+					if (updateData.has("displayedChildren")) {
+						Iterator<JsonElement> displayedIterator = updateData.get("displayedChildren").getAsJsonArray().iterator();
+						List<String> displayedChildren = new ArrayList<>();
+						while (displayedIterator.hasNext()) {
+							String childId = displayedIterator.next().getAsString();
+							displayedChildren.add(childId);
+						}
+						parentItem.setDisplayedChildren(displayedChildren);
 					}
-					parentItem.setDisplayedChildren(displayedChildren);
 				}
-
 				itemList.add(parentItem);
 
-				try (ThumbnailManager thumbMan = new ThumbnailManager()) {
-					thumbMan.updateDirtyBits(parentItem.getId());
-				}
-				success = manager.mergeAll(itemList);
+			} else {
+				log.error("Incoming JSON Object {} has no children");
+				throw new BadRequestException();
+			}
+		}
 
+		// Updating the Items list complete. Now it's time to update that list
+		// in the database
+		if (!itemList.isEmpty()) {
+			
+			// Update the children
+			try (ItemManager manager = new ItemManager()) {
+				updated = manager.mergeAll(itemList);
+			}
+
+			if (updated) {
+				log.info("Updated {} items", itemList.size());
+				// Update the thumbnails
+				try (ThumbnailManager thumbMan = new ThumbnailManager()) {
+					for (Item item : itemList) {
+						thumbMan.updateDirtyBits(item.getId());
+					}
+					log.debug("Updated thumbs for {} items", itemList.size());
+				}
+
+				// Update the status manager
 				try (StatusManager statusMan = new StatusManager()) {
 					Status status = new Status();
 					status.setStatusName(Status.StatusName.STRUCTURE_UPDATE);
-					statusMan.save(status);
+					if (statusMan.save(status)) {
+						log.debug("Status Manager updated structure status after items were updated.");
+					} else {
+						log.warn("Status Manager did not update the structure status after updating items. This could lead to inconsistencies in the data");
+					}
 				}
+			} else {
+				log.warn("Could not update {} items.", itemList.size());
 			}
-		} else {
-			log.error("Incoming JSON Object {} has no children");
-			throw new BadRequestException();
 		}
 
-		return success;
+		return updated;
 	}
 
 }
