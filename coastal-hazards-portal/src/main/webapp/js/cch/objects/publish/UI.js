29f12f36668da6bd4d56b34860555d9a901aa025
Ivan Suftin <isuftin@usgs.gov>
2015-07-23 08:11:56 -0500

    Doing an if-check on CSW response - was causing breaking

--- coastal-hazards-portal/src/main/webapp/js/cch/objects/publish/UI.js
+++ coastal-hazards-portal/src/main/webapp/js/cch/objects/publish/UI.js
@@ -636,10 +636,12 @@ CCH.Objects.Publish.UI = function () {
 						tag = childNode.tag;
 						switch (tag) {
 						case 'spdom':
-							childNode.children[0].children.each(function (spdom) {
-								var direction = spdom.tag.substring(0, spdom.tag.length - 2);
-								$('#form-publish-item-bbox-input-' + direction).val(spdom.text);
-							});
+							if (childNode.children) {
+								childNode.children[0].children.each(function (spdom) {
+									var direction = spdom.tag.substring(0, spdom.tag.length - 2);
+									$('#form-publish-item-bbox-input-' + direction).val(spdom.text);
+								});
+							}
 							break;
 						case 'keywords':
 							childNode.children.each(function (kwNode) {
