4287a249b8275d3bf3bd6b90eb5a493e7b49359d
Carl Schroedl <cschroedl@usgs.gov>
2016-10-19 12:22:03 -0500

    Switching to mockito mocks.

--- coastal-hazards-wps/src/test/java/gov/usgs/cida/coastalhazards/wps/UnzipProcessTest.java
+++ coastal-hazards-wps/src/test/java/gov/usgs/cida/coastalhazards/wps/UnzipProcessTest.java
@@ -16,6 +16,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.ZipInputStream;
 import org.apache.http.Header;
 import org.apache.http.HeaderIterator;
@@ -40,6 +42,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.rules.TemporaryFolder;
+import static org.mockito.Matchers.any;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -106,7 +112,7 @@ public class UnzipProcessTest {
     public void testGetZipFromBadUrl() {
         int[] badCodes = new int[]{ 400, 401, 404, 500, 501};
         for(int badCode : badCodes){
-            HttpClient client = new MockHttpClient(badCode, null);
+            HttpClient client = mockHttpClient(badCode, null);
             try{
                 instance.getZipFromUrl("http://owi.usgs.gov", client);
                 fail("Should throw exception on HTTP error");
@@ -125,11 +131,12 @@ public class UnzipProcessTest {
         int[] goodCodes = new int[]{200, 301, 302, 304, 307};
         byte[] empty = new byte[]{};
         for(int goodCode : goodCodes){
-            HttpClient client = new MockHttpClient(goodCode, new ByteArrayInputStream(empty));
+            HttpClient client = mockHttpClient(goodCode, new ByteArrayInputStream(empty));
             try{
-                instance.getZipFromUrl("http://owi.usgs.gov", client);
+                ZipInputStream zipStream = instance.getZipFromUrl("http://owi.usgs.gov", client);
+                assertNotNull(zipStream);
             } catch (ProcessException ex){
-                fail("Should not throw exception on HTTP error");
+                fail("Should not throw exception on good HTTP status codes");
             }
         }
         return; //No Exceptions were thrown on any HTTP status codes
@@ -142,260 +149,32 @@ public class UnzipProcessTest {
     public void testUnzipToDir() {
         fail("The test case is a prototype.");
     }
-    
-    class MockHttpClient implements HttpClient {
-        int statusCode;
-        InputStream content;
-        public MockHttpClient(int statusCode, InputStream content){
-            this.statusCode = statusCode;
-            this.content = content;
-        }
-        @Override
-        public HttpParams getParams() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public ClientConnectionManager getConnectionManager() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public HttpResponse execute(HttpUriRequest request) throws IOException, ClientProtocolException {
-            return new HttpResponse(){
-                @Override
-                public StatusLine getStatusLine() {
-                    return new StatusLine() {
-                        @Override
-                        public ProtocolVersion getProtocolVersion() {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-
-                        @Override
-                        public int getStatusCode() {
-                            return statusCode;
-                        }
-
-                        @Override
-                        public String getReasonPhrase() {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-                    };
-                }
-
-                @Override
-                public void setStatusLine(StatusLine statusline) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void setStatusLine(ProtocolVersion ver, int code) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void setStatusLine(ProtocolVersion ver, int code, String reason) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void setStatusCode(int code) throws IllegalStateException {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void setReasonPhrase(String reason) throws IllegalStateException {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public HttpEntity getEntity() {
-                    return new HttpEntity(){
-                        @Override
-                        public boolean isRepeatable() {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-
-                        @Override
-                        public boolean isChunked() {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-
-                        @Override
-                        public long getContentLength() {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-
-                        @Override
-                        public Header getContentType() {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-
-                        @Override
-                        public Header getContentEncoding() {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-
-                        @Override
-                        public InputStream getContent() throws IOException, IllegalStateException {
-                            return content;
-                        }
-
-                        @Override
-                        public void writeTo(OutputStream outstream) throws IOException {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-
-                        @Override
-                        public boolean isStreaming() {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-
-                        @Override
-                        public void consumeContent() throws IOException {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-                        
-                    };
-                }
-
-                @Override
-                public void setEntity(HttpEntity entity) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public Locale getLocale() {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void setLocale(Locale loc) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public ProtocolVersion getProtocolVersion() {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public boolean containsHeader(String name) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public Header[] getHeaders(String name) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public Header getFirstHeader(String name) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public Header getLastHeader(String name) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public Header[] getAllHeaders() {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void addHeader(Header header) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void addHeader(String name, String value) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void setHeader(Header header) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void setHeader(String name, String value) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void setHeaders(Header[] headers) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void removeHeader(Header header) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void removeHeaders(String name) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public HeaderIterator headerIterator() {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public HeaderIterator headerIterator(String name) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public HttpParams getParams() {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-
-                @Override
-                public void setParams(HttpParams params) {
-                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                }
-                
-            };
-        }
-
-        @Override
-        public HttpResponse execute(HttpUriRequest request, HttpContext context) throws IOException, ClientProtocolException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public HttpResponse execute(HttpHost target, HttpRequest request) throws IOException, ClientProtocolException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public HttpResponse execute(HttpHost target, HttpRequest request, HttpContext context) throws IOException, ClientProtocolException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public <T> T execute(HttpUriRequest request, ResponseHandler<? extends T> responseHandler) throws IOException, ClientProtocolException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public <T> T execute(HttpUriRequest request, ResponseHandler<? extends T> responseHandler, HttpContext context) throws IOException, ClientProtocolException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public <T> T execute(HttpHost target, HttpRequest request, ResponseHandler<? extends T> responseHandler) throws IOException, ClientProtocolException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public <T> T execute(HttpHost target, HttpRequest request, ResponseHandler<? extends T> responseHandler, HttpContext context) throws IOException, ClientProtocolException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public HttpClient mockHttpClient(int code, InputStream content){
+        
+        
+        StatusLine statusLine = mock(StatusLine.class);
+        when(statusLine.getStatusCode()).thenReturn(code);
+        
+        HttpEntity entity = mock(HttpEntity.class);
+        
+        try {
+            when(entity.getContent()).thenReturn(content);
+        } catch (IOException | IllegalStateException ex) {
+            throw new RuntimeException(ex);
         }
         
+        HttpResponse response = mock(HttpResponse.class);
+        when(response.getStatusLine()).thenReturn(statusLine);
+        when(response.getEntity()).thenReturn(entity);
+        
+        HttpClient client = mock(HttpClient.class);
+        try {
+            when(client.execute(any(HttpUriRequest.class))).thenReturn(response);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        
+        return client;
     }
+    
 }
