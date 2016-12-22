788c44e24c3b8f1de7e162734aa565afafbe577e
Carl Schroedl <cschroedl@usgs.gov>
2016-12-22 14:29:55 -0600

    Splitting out invalidation to a separate script

--- seedAllRasters.sh
+++ seedAllRasters.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e
+# Example usage:
+#./seedAllRasters.sh https://cida-test.er.usgs.gov/dev/coastalchangehazardsportal layer_and_item.csv
+
+#Note that in most cases you will want to run "invalidateRasterCache.sh" before running this script.
+
 CCH_BASE_URL="${1}" #CCH base url without the trailing slash. Ex: https://cida-test.er.usgs.gov/dev/coastalchangehazardsportal
 LAYERS_AND_ITEMS_FILE_NAME="${2}" #Name of a csv file whose first column is the layer name and second column is the item id. The file should not have any header rows. Only one comma should separate the layers and the items -- no spaces should be present. Ex: layers_and_items.csv
-BEARER_ID=${3} #The bearer token from a logged-in session. Ex: e20a4c07-84ac-4a57-90f2-15bda6e8c318
-
-# Example usage:
-#./seedAllRasters.sh https://cida-test.er.usgs.gov/dev/coastalchangehazardsportal layer_and_item.csv e20a4c07-84ac-4a57-90f2-15bda6e8c318
-
-#First invalidate the cache
-curl -v -s -o /dev/null -X DELETE "${CCH_BASE_URL}/data/cache" -H "Authorization: Bearer ${BEARER_ID}"
 
 #Now repopulate the cache
 while IFS='' read -r line || [[ -n "$line" ]]; do
