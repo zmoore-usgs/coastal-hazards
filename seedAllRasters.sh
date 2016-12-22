fa462da21da0c0cd9bebb28b5105fe805496651e
Carl Schroedl <cschroedl@usgs.gov>
2016-12-22 13:57:20 -0600

    Added example usage of wrapper script

--- seedAllRasters.sh
+++ seedAllRasters.sh
@@ -3,6 +3,9 @@ CCH_BASE_URL="${1}" #CCH base url without the trailing slash. Ex: https://cida-t
 LAYERS_AND_ITEMS_FILE_NAME="${2}" #Name of a csv file whose first column is the layer name and second column is the item id. The file should not have any header rows. Only one comma should separate the layers and the items -- no spaces should be present. Ex: layers_and_items.csv
 BEARER_ID=${3} #The bearer token from a logged-in session. Ex: e20a4c07-84ac-4a57-90f2-15bda6e8c318
 
+# Example usage:
+#./seedAllRasters.sh https://cida-test.er.usgs.gov/dev/coastalchangehazardsportal layer_and_item.csv e20a4c07-84ac-4a57-90f2-15bda6e8c318
+
 curl -v -s -o /dev/null -X DELETE "${CCH_BASE_URL}/data/cache" -H "Authorization: Bearer ${BEARER_ID}"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
