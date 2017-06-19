aab882f14c78afac8610edd1edf86748abe5af52
zmoore-usgs <zmoore@usgs.gov>
2017-06-19 15:36:38 -0500

    CCHS-387: Added a method to directly load an item from the alias endpoint.

--- coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/AliasResource.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/rest/data/AliasResource.java
@@ -4,13 +4,14 @@ import com.google.gson.Gson;
 import gov.usgs.cida.coastalhazards.exception.BadRequestException;
 import gov.usgs.cida.coastalhazards.gson.GsonUtil;
 import gov.usgs.cida.coastalhazards.jpa.AliasManager;
+import gov.usgs.cida.coastalhazards.jpa.ItemManager;
+import gov.usgs.cida.coastalhazards.jpa.StatusManager;
 import gov.usgs.cida.coastalhazards.model.Alias;
+import gov.usgs.cida.coastalhazards.model.Item;
+import gov.usgs.cida.coastalhazards.model.util.Status;
 import gov.usgs.cida.coastalhazards.rest.security.CoastalHazardsTokenBasedSecurityFilter;
 import gov.usgs.cida.utilities.HTTPCachingUtil;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Date;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
@@ -59,6 +60,60 @@ public class AliasResource {
 			Alias alias = manager.getAliasForItemId(itemId);
 			Gson gson = GsonUtil.getDefault();
 			response = Response.ok(gson.toJson(alias), MediaType.APPLICATION_JSON_TYPE).build();
+		}
+		return response;
+	}
+	
+	@GET
+	@Path("/{id}/item")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getItemForAlias(@PathParam("id") String id,
+			@DefaultValue("false") @QueryParam("subtree") boolean subtree,
+			@Context Request request) {
+		Response response = null;
+		try (AliasManager aliasManager = new AliasManager();) {
+			Alias alias = aliasManager.load(id);
+			Item item = null;
+			try (StatusManager statusMan = new StatusManager()) {
+				try (ItemManager itemManager = new ItemManager()) {
+					item = itemManager.load(alias.getItemId());
+				}
+				if (item == null) {
+					throw new NotFoundException();
+				} else {
+					// Check when the item and/or structure was last modified, if at all.
+					// - If both are null, use today's date. 
+					// - If one of the two is not null, use that. 
+					// - Else, if both are not null, use the latest between them.
+					Status lastItemUpdate = statusMan.load(Status.StatusName.ITEM_UPDATE);
+					Status lastStructureUpdate = statusMan.load(Status.StatusName.STRUCTURE_UPDATE);
+					Date modified = new Date();
+					if (lastItemUpdate != null && lastStructureUpdate != null) {
+						// Both updates exist, so compare between them and choose the latest
+						Date lastItemUpdateDate = lastItemUpdate.getLastUpdate();
+						Date lastStructureUpdateDate = lastStructureUpdate.getLastUpdate();
+
+						modified = lastItemUpdateDate.after(lastStructureUpdateDate) ? lastItemUpdateDate : lastStructureUpdateDate;
+					} else {
+						// At least one of the two do not exist, so find out if at 
+						// least one exists and use that. 
+						if (lastItemUpdate != null) {
+							modified = lastItemUpdate.getLastUpdate();
+						}
+						if (lastStructureUpdate != null) {
+							modified = lastStructureUpdate.getLastUpdate();
+						}
+					}
+
+					Response unmodified = HTTPCachingUtil.checkModified(request, modified);
+					if (unmodified != null) {
+						response = unmodified;
+					} else {
+						String jsonResult = item.toJSON(subtree);
+						response = Response.ok(jsonResult, MediaType.APPLICATION_JSON_TYPE).lastModified(modified).build();
+					}
+				}
+			}			
 		}
 		return response;
 	}
