63b2e499fd5edfca8dd5a03450f66d2d586774e2
zmoore-usgs <zmoore@usgs.gov>
2018-01-03 16:43:06 -0600

    CCHS-443: Added item aliases to the node text for each item.

--- coastal-hazards-portal/src/main/webapp/js/cch/objects/publish/tree/UI.js
+++ coastal-hazards-portal/src/main/webapp/js/cch/objects/publish/tree/UI.js
@@ -18,14 +18,15 @@ CCH.Objects.Publish.Tree.UI = function (args) {
 
 
 	// The individual tree node.
-	me.createTreeNode = function (item) {
+	me.createTreeNode = function (item, aliases) {
 		var id = item.id,
 			randomId = id === 'uber' || id === 'orphans' ? id : CCH.Util.Util.generateUUID(),
-			text = item.title + (id != 'root' && id != 'uber' && id != 'orphans' ? " <i><small>(" + item.id + ")</small></i>" : ""),
+			text = item.title,
 			itemType = item.itemType,
 			title = item.title,
 			displayedChildren = item.displayedChildren || [],
 			state = {
+				'aliases': aliases,
 				'opened': false,
 				'itemType': itemType,
 				'new-copy': false,
@@ -35,6 +36,17 @@ CCH.Objects.Publish.Tree.UI = function (args) {
 				'displayed': false,
 				'displayedChildren': displayedChildren
 			};
+		
+		//Add additional information to node text
+		if(id !== 'uber' && id !== 'orphans' && id !== 'root') {
+			text += " - <small>( Item ID: " + item.id;
+
+			if(aliases != null && aliases.length > 0) {
+				text += " | Aliases: " + aliases.join(', ');
+			}
+
+			text += " )</small>";
+		}
 
 		return {
 			id: randomId,
@@ -49,9 +61,19 @@ CCH.Objects.Publish.Tree.UI = function (args) {
 	// process where I have to dive into each of the child items to fully build out 
 	// items. The process will call itself many times until I have a fully built out
 	// data set ready to go into the tree UI
-	me.buildAdjacencyListFromData = function (item) {
+	me.buildAdjacencyListFromData = function (item, aliases) {
+		var itemAliases = [];
+
+		if(aliases != null) {
+			aliases.forEach(function(alias) {
+				if(alias.item_id == item.id) {
+					itemAliases.push(alias.id);
+				}
+			});
+		}
+
 		var children = item.children || [],
-				node = this.createTreeNode(item),
+				node = this.createTreeNode(item, itemAliases),
 				referer = CCH.config.referer;
 
 		if (referer && referer === item.id) {
@@ -61,7 +83,7 @@ CCH.Objects.Publish.Tree.UI = function (args) {
 		if (children.length) {
 			for (var childIndex = 0; childIndex < children.length; childIndex++) {
 				var child = children[childIndex];
-				var childNode = this.buildAdjacencyListFromData(child);
+				var childNode = this.buildAdjacencyListFromData(child, aliases);
 
 				if (item.displayedChildren && item.displayedChildren.indexOf(child.id) !== -1) {
 					childNode.state.displayed = true;
@@ -802,35 +824,42 @@ CCH.Objects.Publish.Tree.UI = function (args) {
 	};
 
 	me.init = function () {
-		$.ajax(CCH.config.baseUrl + '/data/tree/item/' + this.id, {
+		//Load all Aliases
+		$.ajax(CCH.config.baseUrl + '/data/alias', {
 			context: this,
-			success: function (item) {
-				var parentItem = {
-					'id': 'root',
-					'itemType': 'root',
-					'text': 'Items',
-					'children': []
-				};
-
-				// First create a data node for the top level item with all children
-				parentItem.children.push(this.buildAdjacencyListFromData(item));
-
-				//Load Orphans
-				$.ajax(CCH.config.baseUrl + '/data/tree/item/orphans/', {
+			success: function (aliases) {
+				//Load Uber and Children
+				$.ajax(CCH.config.baseUrl + '/data/tree/item/' + this.id, {
 					context: this,
 					success: function (item) {
-						var orphanItem = {
-							'id': 'orphans',
-							'itemType': 'aggregation',
-							'title': 'Orphans',
-							'children': item.items
-						}
+						var parentItem = {
+							'id': 'root',
+							'itemType': 'root',
+							'text': 'Items',
+							'children': []
+						};
 
-						// Create a data node for the top level orphan item with all of its children
-						parentItem.children.push(this.buildAdjacencyListFromData(orphanItem));
+						// First create a data node for the top level item with all children
+						parentItem.children.push(this.buildAdjacencyListFromData(item, aliases));
 
-						//Use the retrieved data to create the tree
-						me.createTree([parentItem]);
+						//Load Orphans
+						$.ajax(CCH.config.baseUrl + '/data/tree/item/orphans/', {
+							context: this,
+							success: function (item) {
+								var orphanItem = {
+									'id': 'orphans',
+									'itemType': 'aggregation',
+									'title': 'Orphans',
+									'children': item.items
+								}
+
+								// Create a data node for the top level orphan item with all of its children
+								parentItem.children.push(this.buildAdjacencyListFromData(orphanItem, aliases));
+
+								//Use the retrieved data to create the tree
+								me.createTree([parentItem]);
+							}
+						});
 					}
 				});
 			}
