4a210fa7e6f6182f9369b123f48095aed3dbf8e8
Carl Schroedl <cschroedl@usgs.gov>
2018-04-18 16:39:11 -0500

    Added Alias equals and hashCode methods to support testing.

--- coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/model/Alias.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/model/Alias.java
@@ -3,6 +3,7 @@ package gov.usgs.cida.coastalhazards.model;
 import com.google.gson.Gson;
 import gov.usgs.cida.coastalhazards.gson.GsonUtil;
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -18,7 +19,7 @@ import org.slf4j.LoggerFactory;
 @Entity
 @Table(name = "alias")
 public class Alias implements Serializable {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(Alias.class);
 
 	private String id;
@@ -50,5 +51,34 @@ public class Alias implements Serializable {
 		alias = gson.fromJson(json, Alias.class);
 		
 		return alias;
+	}
+	
+	@Override
+	public int hashCode() {
+		int hash = 7;
+		hash = 83 * hash + Objects.hashCode(this.id);
+		hash = 83 * hash + Objects.hashCode(this.item_id);
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Alias other = (Alias) obj;
+		if (!Objects.equals(this.id, other.id)) {
+			return false;
+		}
+		if (!Objects.equals(this.item_id, other.item_id)) {
+			return false;
+		}
+		return true;
 	}
 }
