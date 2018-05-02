e4534ad5a773e88661097c6ddf05e8de45f531c0
Carl Schroedl <cschroedl@usgs.gov>
2018-05-02 15:06:35 -0500

    Improved documentation, split output between stderr and stdout.

--- util/layer-sweep/find_orphaned_layers.py
+++ util/layer-sweep/find_orphaned_layers.py
@@ -1,6 +1,16 @@
+from __future__ import print_function
 import requests
 import re
 from geoserver.catalog import Catalog
+import sys
+
+def eprint(*args, **kwargs):
+	"""
+	print to stderr instead of stdout
+	In this file, only results inteaded to be piped to additional
+	command	are sent to stdout
+	"""
+	print(*args, file=sys.stderr, **kwargs)
 
 def get_cch_items(cch_url):
 	"""
@@ -8,19 +18,15 @@ def get_cch_items(cch_url):
 	"""
 	#'subtree=false' produces a flat list of all items
 	all_items_url = cch_url + 'data/item?subtree=false&showDisabled=true'
-	print("retrieving CCH items from {}".format(all_items_url))
+	eprint("retrieving CCH items from {}".format(all_items_url))
 	all_items_response = requests.get(all_items_url)
 	items = handle_cch_items_response(all_items_response)
-	print "total cch items retrieved: {}".format(len(items))
-	filtered_items = filter(item_filter, items)
-	print "cch data items that have cch geoserver services: {}".format(len(filtered_items))
-	#return filtered_items
 	return items
 
 def item_filter(item):
 	"""
-	returns true if `item` has 'itemType' == 'data' and has some service endpoints that point to the CCH geoserver
-	
+	returns true if `item` has 'itemType' == 'data' and has some service 
+	endpoints that point to the CCH geoserver
 	"""
 	cch_geoserver_services = get_only_cch_geoserver_services(item['services'])
 	has_cch_geoserver_services = 0 != len(cch_geoserver_services)
@@ -29,7 +35,9 @@ def item_filter(item):
 
 def get_only_cch_geoserver_services(services):
 	"""
-	Filters the parameterized list of services (dicts). The returned list should only contain services (dicts) that have `endpoint`s that look like they are from a cch geoserver.
+	Filters the parameterized list of services (dicts). The returned list 
+	should only contain services (dicts) that have `endpoint`s that look
+	like they are from a cch geoserver.
 	"""
 	#match hostnames of format:
 	# cida-eros-...
@@ -53,9 +61,28 @@ def get_only_cch_geoserver_services(services):
 			)
  	]
 
+def strip_workspace_from_layer_name(layer_name):
+	if ':' in layer_name:
+		workspace, name = layer_name.split(':')
+		return name
+	else:
+		return layer_name
+
+def convert_cch_items_to_cch_geoserver_layer_names(cch_items):
+	layer_names = set()
+	for item in cch_items:
+		cch_geoserver_services = get_only_cch_geoserver_services(item['services'])
+		for service in cch_geoserver_services:
+			layer_name = strip_workspace_from_layer_name(service['serviceParameter'])
+			layer_names.add(layer_name)
+			
+	return layer_names
+
 def handle_cch_items_response(response):
 	"""
-	given a response from a 'request' library request, return a list of dicts. Each dict is a cch item. Raises an Exception if the HTTP response is bad.
+	given a response from a 'request' library request, return a list of 
+	dicts. Each dict is a cch item. Raises an Exception if the 
+	HTTP response is bad.
 	"""
 	if 200 != response.status_code:
 		raise Exception('Could not retrieve CCH Items. Got status code {}'.format(response.status_code))
@@ -69,28 +96,48 @@ def get_geoserver_layers(geoserver_url, geoserver_username, geoserver_password):
 	returns an iterable of GeoServer layers
 	"""
 	geoserver_rest_url = geoserver_url + 'rest'
-	print("retrieving GeoServer layer info via {}".format(geoserver_rest_url))
+	eprint("retrieving GeoServer layer info via {}".format(geoserver_rest_url))
 	cat = Catalog(geoserver_rest_url, username=geoserver_username, password=geoserver_password)
 	all_layers = cat.get_layers()
-	print("total GeoServer layers retrieved: {}".format(len(all_layers)))
 	return all_layers
 
-def find_gs_layers_missing_from_cch_items(gs_layers, cch_items):
+def filter_out_geoserver_layers_that_are_registered(all_gs_layers, registered_geoserver_layer_names):
 	"""
-	returns an iterable of dicts representing GeoServer Layers in `gs_layers` that have no corresponding entry in `cch_items`.
+	all_gs_layers - an iterable of geoserver.layer.Layer
+	registered_geoserver_layer_names - a set of the string layer names
+		that are referenced in items in the CCH portal. The names 
+		should not contain workspaces. In other words they should be 
+		'ABCD' rather than 'proxied:ABCD'
+
+	returns a list of geoserver.layer.Layer objects that are not registered
 	"""
-	pass
+	orphaned_gs_layers = []
+	for layer in all_gs_layers:
+		if layer.name not in registered_geoserver_layer_names:
+			orphaned_gs_layers.append(layer)
+
+	return orphaned_gs_layers
 
 def find_orphaned_layers(geoserver_url, geoserver_username, geoserver_password, cch_url):
 	"""
-	returns an iterable of dicts representing layers that are present in GeoServer, but absent from CCH.
+	returns an iterable of geoserver.layer.Layer objects that are present 
+	in GeoServer, but absent from CCH.
 	"""
-	gs_layers = get_geoserver_layers(geoserver_url, geoserver_username, geoserver_password)
-	#gs_names_to_layers = map_names_to_gs_layers(gs_layers)
+	all_gs_layers = get_geoserver_layers(geoserver_url, geoserver_username, geoserver_password)
 	
 	cch_items = get_cch_items(cch_url)
-	cch_item_ids = map(lambda i: i['id'], cch_items)
-	return cch_items
+	registered_geoserver_layer_names = convert_cch_items_to_cch_geoserver_layer_names(cch_items)
+	orphaned_gs_layers = filter_out_geoserver_layers_that_are_registered(all_gs_layers, registered_geoserver_layer_names)
+	eprint(
+		("\nTotal Layers Retrieved From GeoServer: {}\n" +
+		"Total Items Retrieved From CCH Portal: {}\n" +
+		"Total Orphaned Items: {}\n").format(
+			len(all_gs_layers),
+			len(cch_items),
+			len(orphaned_gs_layers)
+		)
+	)	
+	return orphaned_gs_layers
 	
 def parse_cmd_args(argv):
 	"""
@@ -113,7 +160,9 @@ def parse_cmd_args(argv):
 
 def main(argv):
 	parsed_args = parse_cmd_args(argv)
-	return find_orphaned_layers(**parsed_args)
+	orphaned_layers = find_orphaned_layers(**parsed_args)
+	orphaned_layer_names = '\n'.join([ l.name for l in orphaned_layers ])
+	return orphaned_layer_names
 
 if '__main__' == __name__ :
 	import sys
