c4cbcbf51c54a3300601aeaa82a296199477b429
Ivan Suftin <isuftin@gmail.com>
2018-08-02 10:56:18 -0500

    - isuftin@usgs.gov - travis configuration to build and scan docker container for rserve - isuftin@usgs.gov - maintainer label for rserve dockerfile - isuftin@usgs.gov - healthceck to rserve dockerfile - isuftin@usgs.gov - this changelog
     - isuftin@usgs.gov - rserve package dependencies
     - isuftin@usgs.gov - Switched rserve container to use rocker/r-ver:3.4.3 to lock down
      changes in base container
    
     - isuftin@usgs.gov - processing to change the rserve password file inline

--- Docker/rserve/run.sh
+++ Docker/rserve/run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Restore config from original so replacement keywords are restored
-rm /rserve.pwd
-cp /rserve-original.pwd /rserve.pwd
-
-# Replace Config File Strings
-sed -i -e "s/%RSERVE_PASSWORD%/${RSERVE_PASSWORD}/g" /rserve.pwd
-
 # Launch RServe With Hazard Items
-R -e "library(Rserve);library(hazardItems);setBaseURL('https://${PORTAL_HOST}:${PORTAL_HTTPS_PORT}/coastal-hazards-portal/');run.Rserve(config.file=\"/rserve.conf\")";
+R -e "library(Rserve);library(hazardItems);setBaseURL('https://${PORTAL_INTERNAL_HOST}:${PORTAL_INTERNAL_HTTPS_PORT}/coastal-hazards-portal/');run.Rserve(config.file=\"/rserve.conf\")";
