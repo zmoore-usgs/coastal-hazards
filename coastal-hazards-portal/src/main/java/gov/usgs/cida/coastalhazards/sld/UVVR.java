311f25692d741ca4768bf03b97c8005b77afde86
zxue-usgs <58854300+zxue-usgs@users.noreply.github.com>
2020-02-24 13:46:42 -0500

    Update UVVR.java spacing style

--- coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/sld/UVVR.java
+++ coastal-hazards-portal/src/main/java/gov/usgs/cida/coastalhazards/sld/UVVR.java
@@ -14,7 +14,7 @@ public final class UVVR {
 
 	protected static final String[] attrs = new String[]{UVVR};
 	protected static final float[] thresholds = new float[]{-1f, -1f, 0.025f, 0.050f, 0.075f, 0.100f, 0.200f, 0.300f, 0.400f, 0.500f, 0.75f, 1.0f, 1.25f, 1.5f, 1.75f, 2.0f, 999999999f};
-	protected static final String[] colors = {"#A80000","#2B41FF", "#386DFF", "#3B9DFF","#30CFFF",  "#00FFFF","#70FFD2","#A1FFA4", "#C7FF78", "#E7FF4A","#FFFF00",  "#FFD500", "#FFA600", "#FF7B00","#FF4D00",  "#FF0000"};
+	protected static final String[] colors = {"#A80000", "#2B41FF", "#386DFF", "#3B9DFF", "#30CFFF", "#00FFFF", "#70FFD2", "#A1FFA4", "#C7FF78", "#E7FF4A", "#FFFF00", "#FFD500", "#FFA600", "#FF7B00", "#FF4D00", "#FF0000"};
 
 	protected static final String jspPath = "/SLD/bins_polygon.jsp";
 	protected static final String units = "Vulnerability Index";
