e987cd076fe7936529a3655c4042c5595242068f
Zack Moore <zmoore@usgs.gov>
2020-03-06 14:46:36 -0600

    Add hashbangs

--- cleanIntegrationTestResults.sh
+++ cleanIntegrationTestResults.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo rm -rf ./coastal-hazards-integration-testing/test_results/report
 sudo rm -f  ./coastal-hazards-integration-testing/test_results/jmeter.log
 sudo rm -f  ./coastal-hazards-integration-testing/test_results/result_summary.csv
