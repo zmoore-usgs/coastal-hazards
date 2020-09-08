ad77258a3df0d049c66cc93759462a12b7cecc86
zmoore-usgs <zmoore@usgs.gov>
2020-09-08 17:20:44 -0500

    feat: Made KeyCloak config file configurable via ENVs.

---
+++ coastal-hazards-portal/docker/startup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Replace placeholders in keycloak.json with ENVs
+echo "Replacing placeholers in ${CCH_KEYCLOAK_CONFIG_FILE} with ENVs"
+sed -e "s|%%CCH_KEYCLOAK_REALM%%|${CCH_KEYCLOAK_REALM}|" \
+    -e "s|%%CCH_KEYCLOAK_AUTH_URL%%|${CCH_KEYCLOAK_AUTH_URL}|" \
+    -e "s|%%CCH_KEYCLOAK_RESOURCE%%|${CCH_KEYCLOAK_RESOURCE}|" \
+    -e "s|%%CCH_KEYCLOAK_PUBLIC_CLIENT%%|${CCH_KEYCLOAK_PUBLIC_CLIENT}|" \
+    -e "s|%%CCH_KEYCLOAK_SECRET%%|${CCH_KEYCLOAK_SECRET}|" \
+    "${CCH_KEYCLOAK_CONFIG_FILE}" > /temp.xml
+
+mv /temp.xml "${CCH_KEYCLOAK_CONFIG_FILE}"
+
+exit 0
